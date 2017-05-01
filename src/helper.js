aed06c601cbc3f08413a3cb9b43b0dbf338a37b9
kamos1 <keji.amos@gmail.com>
2017-05-01 14:43:13 -0600

    finish iteration 0

--- src/helper.js
+++ src/helper.js
@@ -1,3 +1,18 @@
 export default class DistrictRepository {
+  constructor(data) {
+    this.data = this.cleaner(data);
+  }
 
+  cleaner(data){
+    return data.reduce((acc, value) => {
+      let { Location, TimeFrame, Data } = value;
+      if(!acc[Location]) {
+        acc[Location] = {};
+        acc[Location].data = {}
+      }
+        acc[Location].data[TimeFrame] = Data
+
+      return acc
+    }, {});
+  }
 }
