6f52f63fc985acdeba17df91fdf6b5c4b9fc6016
Spencer Hilvitz <spencer.hilvitz@colorado.edu>
2017-05-01 16:31:27 -0600

    Iteration 0 passing

--- src/helper.js
+++ src/helper.js
@@ -1,3 +1,16 @@
-export default class DistrictRepository {
+import React from 'react';
 
+export default class DistrictRepository {
+  constructor(data) {
+    this.data = this.cleaner(data);
+  }
+
+  cleaner(data) {
+    let newData = data.reduce((acc, next) => {
+      let location = next.Location;
+      !acc[location] ? acc[location] = [next] : acc[location].push(next)
+      return acc;
+    }, []);
+    return newData;
+  }
 }
