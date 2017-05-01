0d5c476d01116139ba007ad8e2f91bbe8348b1d1
Justyna Field <justyna.field@gmail.com>
2017-05-01 17:10:15 -0600

    Iteration 0: pass no-duplicates test

--- src/helper.js
+++ src/helper.js
@@ -1,12 +1,21 @@
 export default class DistrictRepository {
   constructor(data) {
     this.data = this.mappedData(data)
+
   }
 
   mappedData(data) {
-    return data.map((districts) => {
-      return districts
-    })
+      return data.reduce((accu, value) => {
+
+      if(!accu[value.Location]) {
+        accu[value.Location] = 1
+      }
+
+      accu[value.Location]++
+
+      return accu
+    }, {})
+
   }
 
 }
