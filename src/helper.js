46f97e53f045621cb138aff898a09aa07eef2fc1
Spencer Hilvitz <spencer.hilvitz@colorado.edu>
2017-05-02 16:39:20 -0600

    Clean data correctly

--- src/helper.js
+++ src/helper.js
@@ -3,14 +3,23 @@ import React from 'react';
 export default class DistrictRepository {
   constructor(data) {
     this.data = this.cleaner(data);
+    console.log(this.cleaner(data))
   }
 
   cleaner(data) {
     let newData = data.reduce((acc, next) => {
-      let location = next.Location;
-      !acc[location] ? acc[location] = [next] : acc[location].push(next)
-      return acc;
-    }, []);
-    return newData;
+    let { Location, TimeFrame, Data } = next;
+
+    if (!acc[Location]) {
+      acc[Location] = {Location: Location, [TimeFrame]: Data};
+    }
+    acc[Location][TimeFrame] = Data;
+    return acc
+  }, [])
+  return newData;
+}
+
+  findByName(name) {
+    
   }
 }
