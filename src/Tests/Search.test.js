ea30e21deb989b78be102a72366c452d27e1b217
Chris Bandrowsky <chris.bandrowsky@gmail.com>
2017-05-02 16:37:08 -0600

    create components. render search

---
+++ src/Tests/Search.test.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Search from '../Components/Search';
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  // ReactDOM.render(<App />, div);
+});
