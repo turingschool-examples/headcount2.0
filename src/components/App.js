a89f2367e31da149f397a2e2f7e5920cba37b40e
Devin Dixon <devinisafoal@gmail.com>
2017-05-01 18:00:21 -0600

    add start to controls and app classes

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,33 @@
 import React, { Component } from 'react';
-import './App.css';
+import '../App.css';
+import DistrictRepository from '../helper';
+// import CardContainer from './CardContainer';
+import Controls from './Controls';
+import kinderData from '../../data/kindergartners_in_full_day_program.js';
+
+const district = new DistrictRepository(kinderData).data;
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      district: {},
+    }
+  }
+
+  handleSubmit(area) {
+    const county = district[area.location].data;
+    console.log(county);
+    // this.setState({districts: county})
+    // console.log(this.state.districts)
+  }
+
   render() {
     return (
-      <div>Welcome To Headcount 2.0</div>
+      <div>
+        <Controls handleSubmit={this.handleSubmit.bind(this)}/>
+        Welcome To Headcount 2.0
+      </div>
     );
   }
 }
