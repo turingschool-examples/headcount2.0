89e27c437cee09ac12ed5592c0cca4843eea112d
John Binning <john.binning17@gmail.com>
2017-05-03 16:22:21 -0600

    add testing for new helper functions

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,17 +1,17 @@
 /* eslint-disable */
-import React, { Component } from 'react';
-import '../Styles/normalize.css';
-import '../Styles/App.css';
+import React, { Component } from 'react'
+import '../Styles/normalize.css'
+import '../Styles/App.css'
 import Search from './Search.js'
 import CardGrid from './CardGrid.js'
 import DistrictRepository from '../Helpers/helper'
-import kinderData from '../../data/kindergartners_in_full_day_program.js';
+import kinderData from '../../data/kindergartners_in_full_day_program.js'
 
 
 class App extends Component {
   constructor(){
     super()
-    this.schools=new DistrictRepository(kinderData);
+    this.schools=new DistrictRepository(kinderData)
     this.state={
       full: this.schools.data,
       searched : '',
@@ -30,8 +30,8 @@ class App extends Component {
         <CardGrid schools = {this.schools}
                   searched = {this.state.searched}/>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
