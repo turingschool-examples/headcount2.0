f98b1a2fd185dc4906661bd6b567f517915d07be
kamos1 <keji.amos@gmail.com>
2017-05-03 18:26:26 -0600

    complete iteration 4

--- src/helper.js
+++ src/helper.js
@@ -38,4 +38,23 @@ export default class DistrictRepository {
 
     return found;
   }
+
+  findAverage(location) {
+    const school = this.findByName(location);
+    const keys = Object.keys(school.data);
+    const total = keys.reduce((acc, key) => {
+      acc = acc + school.data[key];
+      return acc;
+    },0)/keys.length;
+    return Math.round(total * 1000) / 1000;
+  }
+
+  compareDistrictAverages(location1, location2) {
+    const district1 = this.findAverage(location1);
+    const district2 = this.findAverage(location2);
+    const divided = district1/district2;
+    const compare = Math.round(divided * 1000) / 1000;
+    const result = {[location1.toUpperCase()]: district1, [location2.toUpperCase()]: district2, compared: compare};
+    return result
+  }
 }
