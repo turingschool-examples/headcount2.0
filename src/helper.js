9f0d6c4bb639f8be968cb0159896587ca904ae2f
James Sullivan <jamesfsullivan5@gmail.com>
2017-06-26 16:55:33 -0600

    Add findByName function.

--- src/helper.js
+++ src/helper.js
@@ -1,16 +1,18 @@
 export default class DistrictRepository {
   constructor(data) {
     this.data = this.normalize(data);
+    console.log(this.data);
   }
 
   normalize(data) {
     return data.reduce((dataObj, e) => {
       if (!dataObj[e.Location]) {
-        dataObj[e.Location] = [];
+        dataObj[e.Location] = {
+          location: e.Location,
+          data: {}
+        };
       }
-      dataObj[e.Location].push({ location: e.Location,
-                                 timeFrame: e.TimeFrame,
-                                 data: e.Data});
+      dataObj[e.Location].data[e.TimeFrame] = e.Data;
 
 
       // const keys = Object.keys(e).map(key =>
@@ -18,5 +20,11 @@ export default class DistrictRepository {
       //     ...key.slice(1)].join(''));
       return dataObj;
     }, {})
+  }
+
+  findByName(name) {
+    let lowCaseName = name.toLowerCase();
+    let keys = Object.keys(this.data).map(e => e.toLowerCase())
+    return this.data[name]
   }
 }
