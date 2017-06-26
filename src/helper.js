a02995c1599f42c0184efb09b1f65b82dc121813
coleworsley <cole.s.worsley@gmail.com>
2017-06-26 14:27:32 -0600

    pass first test

--- src/helper.js
+++ src/helper.js
@@ -1,3 +1,7 @@
-export default class DistrictRepository {
+import kinderData from '../data/kindergartners_in_full_day_program';
 
+export default class DistrictRepository {
+  constructor(data) {
+    this.data = data;
+  }
 }
