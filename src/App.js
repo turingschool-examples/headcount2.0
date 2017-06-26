fe08bce82d08404d0e7f8f1ff727b8eb11a40c6f
coleworsley <cole.s.worsley@gmail.com>
2017-06-26 15:33:36 -0600

    add normalize function

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
+import DistrictRepository from './helper';
+import kinderData from '../data/kindergartners_in_full_day_program';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = new DistrictRepository(kinderData);
+  }
+
+
   render() {
     return (
       <div>Welcome To Headcount 2.0</div>
--- src/helper.js
+++ src/helper.js
@@ -1,7 +1,35 @@
-import kinderData from '../data/kindergartners_in_full_day_program';
-
 export default class DistrictRepository {
   constructor(data) {
-    this.data = data;
+    this.data = this.normalize(data);
+  }
+
+  normalize(data) {
+    // return data.reduce((dataObj, e))
+
+
+
+
+    return data.reduce((acc, e, i) => {
+      let index = acc.findIndex(accE => {
+        return accE.location === e.Location
+      })
+
+      if (index === -1) {
+        acc[acc.length] = {
+          location: e.Location,
+          data: [{
+            timeFrame: e.TimeFrame,
+            data: e.Data,
+          }],
+        }
+      } else {
+        acc[index].data.push({
+          timeFrame: e.TimeFrame,
+          data: e.Data,
+        });
+      }
+
+      return acc;
+    }, []);
   }
 }
