67d30387cbd6592577b8407a4b2c2c8e8819ed1d
Travis <tlgreg86@gmail.com>
2017-06-26 16:27:12 -0600

    get iteration 0 tests to pass

--- src/helper.js
+++ src/helper.js
@@ -1,3 +1,20 @@
 export default class DistrictRepository {
+  constructor(data) {
+    this.data = data.reduce((accum, object) => {
+          if(!accum[object.Location]) {
+            accum[object.Location] = 'value'
+          }
+          return accum
+        },{})
+  }
+
+  reducedData(data) {
+    data.reduce((accum, object) => {
+      if(!accum[object.Location]) {
+        accum[object.Location] = 'value'
+      }
+      return accum
+    },{})
+  }
 
 }
