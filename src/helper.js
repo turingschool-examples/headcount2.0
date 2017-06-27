a21ff3f9b5097cd40dc5b36520ced0fa49d8788d
James Sullivan <jamesfsullivan5@gmail.com>
2017-06-27 15:04:16 -0600

    Add find averages function.

--- src/helper.js
+++ src/helper.js
@@ -32,10 +32,32 @@ export default class DistrictRepository {
   }
 
   findAllMatches(name) {
+    // this.findAverage('colorado');
     const lowCaseName = name === undefined ? '' : name.toLowerCase();
     return Object.keys(this.data).filter(e => {
       const index = e.toLowerCase().indexOf(lowCaseName);
       return lowCaseName === '' ? true : index >= 0;
     });
+  }
+
+  findAverage(city) {
+    const cityToAvg = this.findByName(city).data;
+    console.log(cityToAvg);
+    const keys = Object.keys(cityToAvg);
+
+    return Math.round((keys.reduce((accu, elem) => accu + cityToAvg[elem], 0) / keys.length) *1000) / 1000 ;
+
+
+
+
+    return keys.reduce((accu, elem, index, array) => {
+      console.log(elem)
+      accu += cityToAvg[elem];
+      return accu
+    }, 0) / keys.length;
+  }
+
+  compareDistrictAverages() {
+
   }
 }
