f99479d2b0b0f4e630c1b740b85f6682381f77e8
James Sullivan <jamesfsullivan5@gmail.com>
2017-06-28 17:05:35 -0600

    Add Card testing - finished.

--- test/component/Card-test.test.js
+++ test/component/Card-test.test.js
@@ -14,6 +14,31 @@ const city = {
 
 it('Should mount', () => {
   const wrapper = shallow(<Card city={city}/>)
-  console.log(wrapper.debug());
   expect(wrapper.find('.card').length).toEqual(1);
+})
+
+it('should display a loction', () => {
+  const wrapper = shallow(<Card city={city}/>)
+  expect(wrapper.contains(<h1>fake place</h1>)).toEqual(true);
+})
+
+it('should display all the yearly data given', () => {
+  const wrapper = shallow(<Card city={city}/>)
+  expect(wrapper.find('tr').length).toEqual(3)
+})
+
+it('should should give className of "red" to years with number of .5 or lower and green if above .5 ', () => {
+  const wrapper = shallow(<Card city={city}/>)
+  expect(wrapper.find('.red').length).toEqual(1)
+  expect(wrapper.find('.green').length).toEqual(2)
+})
+
+it('Should run clickActive when clicked', () => {
+  const mockFn = jest.fn();
+  const wrapper = mount(<Card city={city} clickActive={mockFn}/>)
+  const card = wrapper.find('.card');
+  expect(mockFn).toHaveBeenCalledTimes(0);
+  card.simulate('click');
+  expect(mockFn).toHaveBeenCalledTimes(1);
+
 })
