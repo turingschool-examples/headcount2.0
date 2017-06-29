b3b1405336391c766988de14e7d627c8fddbf044
coleworsley <cole.s.worsley@gmail.com>
2017-06-29 16:25:09 -0600

    add graph re-sizing

--- src/Graph.js
+++ src/Graph.js
@@ -2,51 +2,82 @@ import React, { Component } from 'react';
 import { LineChart, Line, ScatterChart, Scatter, CartesianGrid, Tooltip, Legend,
  XAxis, YAxis, ZAxis, ReferenceLine, ReferenceDot, ReferenceArea, ErrorBar } from 'recharts';
 
-const Graph = ({data}) => {
-  const keys = Object.keys(data);
-  let dataNorm = [];
-  let dataNorm2 = [];
-  if (data.length === 2) {
-    dataNorm = Object.keys(data[0].data).map((e, i, arr)=> {
-      return {
-        year: parseInt(e),
-        data: data[0].data[e],
-      }
-    });
-    dataNorm2 = Object.keys(data[1].data).map((e, i, arr)=> {
-      return {
-        year: parseInt(e),
-        data: data[1].data[e],
-      }
-    });
+
+export default class Graph extends Component {
+  constructor() {
+    super();
+    this.state = {
+      height: 300,
+      width: 600,
+    }
   }
 
-  return (
-    <ScatterChart width={500}
-                  height={300}
-                  margin={{ top: 20, right: 20, bottom: 0, left: 20 }}>
-      <XAxis type="number"
-             dataKey="year"
-             domain={['dataMin', 'dataMax']}
-             name="year" />
-      <YAxis dataKey="data"
-             name="data"/>
-      <CartesianGrid />
-      <Tooltip cursor={
-          { stroke: '#808080',
-            strokeDasharray: '5 5' }}/>
-      <Legend/>
-      <Scatter line lineJointType="monotoneX"
-               shape="circle"
-               legendType="circle"
-               data={dataNorm}
-               fill="#ff7300" />
-      <Scatter line shape="square"
-               legendType="square"
-               data={dataNorm2}
-               fill="#347300" />
-    </ScatterChart>
-  );
-}
+  componentDidMount() {
+    const parent = this.refs.graph.parentNode
+    const width = parent.clientWidth - 50;
+    const height = parent.clientHeight - 50;
 
-export default Graph;
+    this.setState({
+      height,
+      width,
+    })
+  }
+
+  render() {
+    const { width, height } = this.state;
+    const { data } = this.props;
+    const keys = Object.keys(data);
+    let dataNorm = [];
+    let dataNorm2 = [];
+    if (data.length === 2) {
+      dataNorm = Object.keys(data[0].data).map((e, i, arr)=> {
+        return {
+          year: parseInt(e),
+          data: data[0].data[e],
+        }
+      });
+      dataNorm2 = Object.keys(data[1].data).map((e, i, arr)=> {
+        return {
+          year: parseInt(e),
+          data: data[1].data[e],
+        }
+      });
+    }
+
+    return (
+      <div ref='graph'>
+        <ScatterChart
+          width={width}
+          height={height}
+          margin={{ top: 20, right: 20, bottom: 0, left: 20 }}>
+          <XAxis type="number"
+            dataKey="year"
+            domain={['dataMin', 'dataMax']}
+            name="year" />
+          <YAxis dataKey="data"
+            name="data"/>
+          <CartesianGrid />
+          <Tooltip cursor={
+              { stroke: '#808080',
+                strokeDasharray: '5 5' }}/>
+              <Legend/>
+              <Scatter line lineJointType="monotoneX"
+                shape="circle"
+                legendType="circle"
+                data={dataNorm}
+                fill="#ff7300" />
+              <Scatter line shape="square"
+                legendType="square"
+                data={dataNorm2}
+                fill="#347300" />
+            </ScatterChart>
+
+      </div>
+    );
+  }
+}
+// const Graph = ({data}) => {
+//
+// }
+//
+// export default Graph;
