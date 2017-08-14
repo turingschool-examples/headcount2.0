896236ea5544fba79656b3c68203d79d8d1bf546
Tyler Hevia <tylerhevia@gmail.com>
2017-08-14 14:36:54 -0600

    finished iteration 0

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import "./App.css";
+import DistrictRepository from "./DistrictRepository";
+
+class App extends Component {
+  render() {
+    return <div>Welcome To Headcount 2.0</div>;
+  }
+}
+
+export default App;
