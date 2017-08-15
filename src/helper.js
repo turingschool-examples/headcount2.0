8b3c4975c3a0d311e80f683eb03c242e02a158c9
sljohnson32 <sljohnson32@gmail.com>
2017-08-15 08:58:27 -0600

    add data handler in helper

--- src/helper.js
+++ src/helper.js
@@ -1,3 +1,23 @@
 export default class DistrictRepository {
+  constructor(data) {
+    this.data = this.cleanData(data);
+  }
+
+  cleanData = (rawData) => {
+    return rawData.reduce((obj, location) => {
+        const keys = Object.keys(location);
+        const formattedKeys = this.getKeys(keys);
+        const dataObj = this.getDataObj(keys, location);
+    }, {});
+  }
+
+  getDataObj = (keys, location) = {
+    keys.reduce((obj, key, i) => {
+      if (i != 0) {
+        obj[key] = location[keys[i]];
+      }
+      return obj;
+    }, {}
+  }
 
 }
