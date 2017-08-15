f878fb36963216287f81bf284cd7208bd6b712d6
sljohnson32 <sljohnson32@gmail.com>
2017-08-15 17:11:57 -0600

    Finish interation 0 and add find name function

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
+import DistrictRepository from './helper.js'
+import kinderData from '../data/kindergartners_in_full_day_program.js';
 
 class App extends Component {
+
   render() {
+    let districtData = new DistrictRepository(kinderData)
+    console.log(districtData.findByName('Academy 20'))
     return (
       <div>Welcome To Headcount 2.0</div>
     );
--- src/helper.js
+++ src/helper.js
@@ -3,21 +3,44 @@ export default class DistrictRepository {
     this.data = this.cleanData(data);
   }
 
-  cleanData = (rawData) => {
+  cleanData(rawData) {
     return rawData.reduce((obj, location) => {
-        const keys = Object.keys(location);
-        const formattedKeys = this.getKeys(keys);
-        const dataObj = this.getDataObj(keys, location);
+      // const keys = Object.keys(location);
+      const dataObj = this.getDataObj(location);
+      const loc = location.Location;
+      if (!obj[loc]) {
+        obj[loc] = {
+          location: loc,
+          data: []
+        }
+      }
+      obj[loc].data.push(dataObj)
+      return obj
     }, {});
   }
 
-  getDataObj = (keys, location) = {
-    keys.reduce((obj, key, i) => {
-      if (i != 0) {
-        obj[key] = location[keys[i]];
-      }
-      return obj;
-    }, {}
+  getDataObj(location) {
+    let newObj = {}
+    newObj[location.TimeFrame] = this.getRoundedNum(location.Data)
+    return newObj;
   }
 
+  getRoundedNum(num) {
+    if (isNaN(num)) {
+      return 0;
+    }
+    return Math.round(1000 * num)/1000;
+  }
+
+  findByName(input) {
+    debugger
+    if (!input) {
+      return undefined
+    }
+    let initialLocations = Object.keys(this.data);
+    let cleanedLocations = initialLocations.map(key => key.toLowerCase());
+    let searchIndex = cleanedLocations.indexOf(input.toLowerCase());
+
+    return this.data[initialLocations[searchIndex]]
+  }
 }
