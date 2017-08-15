2bc11bfd8029a33a861e46e7313fcb2fc8380202
Travis Rollins <TROLL59910@aol.com>
2017-08-14 21:41:45 -0600

    Refactored getData method and fixed bug so district data shows correctly

--- src/DistrictRepository.js
+++ src/DistrictRepository.js
@@ -1,49 +1,41 @@
 import React, { Component } from 'react'
-import kinderData from '../data/kindergartners_in_full_day_program'
 
 export default class DistrictRepository extends Component {
-  constructor() {
+  constructor(data) {
     super();
-    this.data = this.getData(kinderData);
+    this.data = this.getData(data);
   }
-
-  // componentDidMount() {
-  //   this.getData(kinderData)
-  // }
 
   getData(data) {
     return data.reduce((accu, elem) => {
+      const newObj = Object.assign({}, elem, { Location: elem.Location.toUpperCase(), Data: {} } );
 
       if (!accu[elem.Location]) {
-
-        accu[elem.Location.toUpperCase()] = Object.assign(elem, { Location: elem.Location.toUpperCase() } )
-        // accu[elem.Location].Data = { elem.Data}
+        accu[elem.Location] = newObj
       }
 
-      const roundedData = Math.round(1000*elem.Data)/1000
-      accu[elem.Location].Data = { [elem.TimeFrame]: roundedData };
-      // console.log(accu[elem.Location].Data[elem.TimeFrame]);
+      const accuData = accu[elem.Location].Data;
+      const roundedData = Math.round(1000*elem.Data)/1000;
+
+      Object.assign(accuData, {[elem.TimeFrame]: roundedData})
       return accu
     }, {})
   }
 
   findByName(name) {
-    // console.log(this.data);
     if (name === undefined) {
       return undefined
     }
 
     const upName = name.toUpperCase();
     const dataKey = Object.keys(this.data)
-    const searchedKey = dataKey.filter( (key) => upName === key )
+    const searchedKey = dataKey.filter( (key) => upName === key.toUpperCase() )
     return this.data[searchedKey]
   }
 
   render() {
     return (
       <div>
-        <button onClick={() => this.data(kinderData)}>
-        </button>
       </div>
     )
   }
