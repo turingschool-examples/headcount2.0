e27dc60a5317208a5b413c4b06a6706b81b13b10
Jonathan Beckman <jbecks.media@gmail.com>
2017-08-15 17:58:03 -0600

    build out test for schoolCard, add classNames and complete average functionality

--- src/SchoolCard.js
+++ src/SchoolCard.js
@@ -1,16 +1,21 @@
 import React, { Component } from 'react';
 
-const SchoolCard = ({data}) => {
+const SchoolCard = ({ data }) => {
   const {Location, Data} = data;
-  const yearsKeys = Object.keys(Data)
-  const percentages = yearsKeys.map(year => <li>{year}: {Data[year]}</li>)
+  const yearsKeys = Object.keys(Data);
+  const listItems = yearsKeys.map( (year, i) => <li key={i} className="year-data">{year}: {Data[year]}</li>);
+  const averages = yearsKeys.reduce( (average, year) => {
+    average += Data[year]
+    return average
+  }, 0) / yearsKeys.length
+  const roundedAverages = Math.round(100*averages)/100;
 
   return (
     <div>
-      <h3>{Location}</h3>
-      <p>Average</p>
-      <ul>
-        {percentages}
+      <h3 className="school-name">{Location}</h3>
+      <p className="average">{roundedAverages}</p>
+      <ul className="year-percentages">
+        {listItems}
       </ul>
     </div>
   )
