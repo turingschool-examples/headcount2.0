582e76abb316051565f5107f7a89bc5e87bdd3c9
Jack Mallahan <jackmallahan@gmail.com>
2017-08-16 17:55:00 -0600

    lick my butt github

---
+++ src/components/DistrictRepository.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+
+export default class DistrictRepository extends Component {
+	constructor(rawData) {
+		super();
+		this.data = this.getDistrictData(rawData);
+	}
+
+	getDistrictData(rawData) {
+		const cleanData = rawData.reduce((acc, obj) => {
+			if (!acc[obj.Location]) {
+				acc[obj.Location] = [];
+			}
+			if (acc[obj.Location]) {
+				acc[obj.Location].push(obj);
+			}
+			return acc;
+		}, {});
+		return cleanData;
+	}
+
+	getData(districtInfo) {
+		districtInfo = districtInfo.reduce((acc, item) => {
+			if (item.Data === 'N/A') {
+				item.Data = 0;
+			}
+			acc[item.TimeFrame] = parseFloat(item.Data.toFixed(3));
+			return acc;
+		}, {});
+		return districtInfo;
+	}
+
+	findByName(userInput) {
+		if (userInput) {
+			userInput = userInput.toUpperCase();
+		}
+		const dataSet = this.data;
+		const districtKeys = Object.keys(dataSet).map(key => {
+			return key.toUpperCase();
+		});
+
+		let districtName = dataSet[userInput];
+		let answer;
+
+		if (districtKeys.includes(userInput)) {
+			return (answer = {
+				location: userInput,
+				data: this.getData(districtName)
+			});
+		}
+	}
+}
