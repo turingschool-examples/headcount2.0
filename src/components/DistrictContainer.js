ecbaa23dc677fc75c26bd66303258ee2b1e0031c
Jack Mallahan <jackmallahan@gmail.com>
2017-08-16 18:40:23 -0600

    added unique keys for every rendered child

--- src/components/DistrictContainer.js
+++ src/components/DistrictContainer.js
@@ -1,19 +1,20 @@
-import React from "react";
-import { DistrictCard } from "./DistrictCard";
-import "../styles/DistrictRepository.css";
+import React from 'react';
+import { DistrictCard } from './DistrictCard';
+import '../styles/DistrictRepository.css';
 
 const DistrictContainer = props => {
-  return (
-    <div className="district-container">
-      {props.foundData.map(place =>
-        <DistrictCard
-          location={place}
-          data={props.fullData[place]}
-          getData={props.getData}
-        />
-      )}
-    </div>
-  );
+	return (
+		<div className="district-container">
+			{props.foundData.map(place =>
+				<DistrictCard
+					location={place}
+					data={props.fullData[place]}
+					getData={props.getData}
+					key={Math.random()}
+				/>
+			)}
+		</div>
+	);
 };
 
 export default DistrictContainer;
