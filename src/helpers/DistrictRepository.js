43b63d2dd6f34154edf38ad41d75856f9d929664
Jack Mallahan <jackmallahan@gmail.com>
2017-08-16 18:30:42 -0600

    getting our search feature hooked up

--- src/helpers/DistrictRepository.js
+++ src/helpers/DistrictRepository.js
@@ -1,76 +1,74 @@
-import React, { Component } from "react";
-import KinderData from "../../data/kindergartners_in_full_day_program";
-import "../styles/DistrictRepository.css";
+import React, { Component } from 'react';
+import KinderData from '../../data/kindergartners_in_full_day_program';
+import '../styles/DistrictRepository.css';
 
 export class DistrictRepository {
-  constructor(data) {
-    this.data = data;
-  }
+	constructor(data) {
+		this.data = data;
+	}
 
-  cleanData(districtInfo) {
-    districtInfo = districtInfo.reduce((acc, item) => {
-      if (item.Data === "N/A") {
-        item.Data = 0;
-      }
-      acc[item.TimeFrame] = parseFloat(item.Data.toFixed(3));
-      return acc;
-    }, {});
-    return districtInfo;
-  }
+	cleanData(districtInfo) {
+		districtInfo = districtInfo.reduce((acc, item) => {
+			if (item.Data === 'N/A') {
+				item.Data = 0;
+			}
+			acc[item.TimeFrame] = parseFloat(item.Data.toFixed(3));
+			return acc;
+		}, {});
+		return districtInfo;
+	}
 
-  findAllMatches(userInput) {
-    if (userInput) {
-      userInput = userInput.toUpperCase();
-    }
+	findAllMatches(userInput) {
+		if (userInput) {
+			userInput = userInput.toUpperCase();
+		}
 
-    const dataSet = this.getDistrictData();
-    const districtKeys = Object.keys(dataSet).map(key => {
-      return key.toUpperCase();
-    });
+		const dataSet = this.getDistrictData();
+		const districtKeys = Object.keys(dataSet).map(key => {
+			return key.toUpperCase();
+		});
 
-    if (!userInput) {
-      return districtKeys;
-    }
+		if (!userInput) {
+			return districtKeys;
+		}
 
-    let filteredMatches = districtKeys.filter(district =>
-      district.includes(userInput)
-    );
+		let filteredMatches = districtKeys.filter(district =>
+			district.includes(userInput)
+		);
 
-    return filteredMatches;
-  }
+		return filteredMatches;
+	}
 
-  findByName(userInput) {
-    if (userInput) {
-      userInput = userInput.toUpperCase();
-    }
-    let newData = this.getDistrictData(this.data);
-    const districtKeys = Object.keys(newData).map(key => {
-      return key.toUpperCase();
-    });
+	findByName(userInput) {
+		if (userInput) {
+			userInput = userInput.toUpperCase();
+		}
+		let newData = this.getDistrictData(this.data);
+		const districtKeys = Object.keys(newData).map(key => {
+			return key.toUpperCase();
+		});
 
-    let districtName = newData[userInput];
-    let answer;
+		let districtName = newData[userInput];
+		let answer;
 
-    if (districtKeys.includes(userInput)) {
-      return (answer = {
-        location: userInput,
-        data: this.cleanData(districtName)
-      });
-    }
-  }
+		if (districtKeys.includes(userInput)) {
+			return (answer = {
+				location: userInput,
+				data: this.cleanData(districtName)
+			});
+		}
+	}
 
-  getDistrictData() {
-    const cleanData = this.data.reduce((acc, obj) => {
-      if (!acc[obj.Location]) {
-        acc[obj.Location] = [];
-      }
-      if (acc[obj.Location]) {
-        acc[obj.Location].push(obj);
-      }
-      return acc;
-    }, {});
-    return cleanData;
-  }
+	getDistrictData() {
+		const cleanData = this.data.reduce((acc, obj) => {
+			if (!acc[obj.Location]) {
+				acc[obj.Location] = [];
+			}
+			if (acc[obj.Location]) {
+				acc[obj.Location].push(obj);
+			}
+			return acc;
+		}, {});
+		return cleanData;
+	}
 }
-
-// export default DistrictRepository;
