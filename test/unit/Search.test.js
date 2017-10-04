3f70aed94c83d2532dd0e5c3e6e6fc12cf8f288a
Hector Sanchez <hsanchez7934@gmail.com>
2017-10-04 14:37:40 -0600

    add describe block, before each, and write first test to make sure state.input is changing

--- test/unit/Search.test.js
+++ test/unit/Search.test.js
@@ -2,3 +2,20 @@
 // simple tests such as it should be defined
 // it should render text input field
 // call a function in app on input change
+import React from 'react';
+import Search from '../../src/Search';
+import { shallow, mount } from 'enzyme';
+
+
+
+describe('Search Component', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallow(<Search />);
+  });
+
+  it('updates state.input when a user types in to the search', () => {
+    
+  });
+});
