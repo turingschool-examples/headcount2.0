3d4b43d60d77c71e198e4f8ba8ab79e15ce73271
Hector Sanchez <hsanchez7934@gmail.com>
2017-10-04 08:51:28 -0600

    initialize search component and add handle change func to search component, add cardsearch function to app component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react';
+import Search from './Search';
 import './styles/App.css';
 
 class App extends Component {
+
+
+  cardSearch(query) {
+    console.log(query);
+  }
+
   render() {
     return (
-      <div>Welcome To Headcount 2.0</div>
+      <div>Welcome To Headcount 2.0
+        <Search cardSearch={this.cardSearch}/>
+
+      </div>
     );
   }
 }
