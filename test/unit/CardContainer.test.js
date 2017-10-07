dd4623c9bdf2dfaafeb6eb95a72a7215015ea1a0
Hector Sanchez <hsanchez7934@gmail.com>
2017-10-07 15:49:00 -0600

    add card container test folder

---
+++ test/unit/CardContainer.test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import CardContainer from '../../src/CardContainer';
+// import App from '../../src/App';
+import { shallow, mount } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-15';
+import config from '../setup.js';
+import DistrictRepository from '../../src/helper.js';
+import kinderData from '../../data/kindergartners_in_full_day_program.js';
+
+describe('CardContainer unit testing', () => {
+
+});
