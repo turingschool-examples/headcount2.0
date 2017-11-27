90f2eac2d8e80bcef97ff2b5a953fc642837bfe7
Hugh Hartigan <Hartigan.HM@gmail.com>
2017-11-27 15:17:08 -0700

    Built cleanData function for helper.js

--- src/helper.js
+++ src/helper.js
@@ -1,5 +1,17 @@
 export default class DistrictRepository {
     constructor(data) {
-        this.data = data;
+        this.data = this.cleanData(data);
+    }
+
+    cleanData(data) {
+        let newData = data.reduce( (newData, object) => {
+            if(!newData[object.Location]) {
+                newData[object.Location] = {}
+            }
+
+            return newData;
+        }, {})
+
+        return newData;
     }
 }
