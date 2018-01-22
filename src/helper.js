8feb8df9cf9a34a4a4b9c9e5b00a27b7191ceaf7
Bruce Chung <brucekchung@gmail.com>
2018-01-22 16:24:26 -0700

    Initial commit, iteration-0 test pass

--- src/helper.js
+++ src/helper.js
@@ -1,3 +1,26 @@
 export default class DistrictRepository {
+  constructor(data) {
+    this.data = this.formatData(data)
+  }
+
+  //  obj = {
+  //    colorodo: {
+  //                location: COLORODO, 
+  //                data: {year: percentage, year: percentage} 
+  //              }
+  //  }
+
+  formatData(data) {
+    //sort years and percentages within each dataset 2004-2014
+
+    return data.reduce((obj, item) => {
+      //make key
+      if (!obj[item.Location]) {
+        obj[item.Location] = {}
+      }
+      return obj
+    }, {})
+  }
+
 
 }
