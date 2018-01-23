3429752f9ae21a965b448ed9af47d1f69f2b5582
parker lindley <fugazi8@gmail.com>
2018-01-22 21:30:53 -0700

    kind of working

--- src/helper.js
+++ src/helper.js
@@ -2,12 +2,35 @@ import React from 'react';
 
 export default class DistrictRepository {
   constructor(data) {
-    this.data = data.reduce((obj, district) => {
-                  if (!obj[district.Location]) {
-                    obj[district.Location] = []
-                  }
-                  obj[district.Location].push({year: district.TimeFrame, avg: district.Data})
-                  return obj;
-                }, {})
+    this.data = 
+    data.reduce((obj, district) => {
+      district.Location = district.Location.toUpperCase()
+      if (!obj[district.Location]) {
+        obj[district.Location] = {}
+      }
+      //obj[district.Location].location = district.Location;
+
+      obj[district.Location] = {
+        ...obj[district.Location],
+         [district.TimeFrame]: district.Data
+      }
+
+      return obj;
+    }, {})
+  }
+
+  findByName(search) {
+    if (search === undefined) {
+      return undefined
+    } else {
+      let searched = search.toUpperCase();
+
+      if (!this.data[searched]) {
+        return undefined
+      } else {
+        console.log(this.data[searched])
+        return this.data[searched]
+      }
+    }
   }
 }
