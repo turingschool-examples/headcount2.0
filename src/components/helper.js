c75a62c51a3f61d8315d5273558a93d69d076b5a
Ricardo Viera <ricardoclean@gmail.com>
2018-01-23 16:54:35 -0700

    Iteration 1
    Added data manipulation and cleaned the data based on school district location names.
    Added a findByName method that returns an object based on district location names.

--- src/components/helper.js
+++ src/components/helper.js
@@ -4,14 +4,26 @@ export default class DistrictRepository {
   }
 
   schoolData(info) {
+      
     return info.reduce((accu, school) => {
-      if(!accu[school.Location]) {
-        accu[school.Location] = {}
-        // started looking at how to store duplicated data in our object
-        // accu[school.Location] = {TimeFrame:school.TimeFrame}
-      }
 
+      if(!accu[school.Location]) {
+        accu[school.Location.toUpperCase()] = {data:{}, location: school.Location.toUpperCase()}
+        }
+        accu[school.Location.toUpperCase()].data[school.TimeFrame] = isNaN(school.Data) ? 0 : Math.round(school.Data * 1000) / 1000 
+        
       return accu;
-    }, {})
+    }, [])
+    
   }  
+
+  findByName(districtName){
+    
+    if(districtName) {
+      let upperCase = districtName.toUpperCase();
+      return this.data[upperCase]
+    } else {
+      return undefined
+    }
+  }
 }
