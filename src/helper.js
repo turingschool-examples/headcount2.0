b7a646a2801afa9c9c44cd592cc90941f5ea55e2
Amy Russell <russell.amyh@gmail.com>
2018-01-22 19:59:57 -0700

    Create DistrictRepository class, passing in data and then creating a new object with keys of location and values of data

--- src/helper.js
+++ src/helper.js
@@ -1,3 +1,32 @@
 export default class DistrictRepository {
+  constructor(data) {
+    this.data = this.dataCleaner(data)
+
+
+  }
+
+  dataCleaner(data) {
+
+    let cleanData = data.reduce( (districtObject, district)=> {
+    
+    // if there's no district in the districtObject
+    // with that location, create a key with that location
+    // the value of that key should be an array of 
+    // all the districts that have matching locations
+      let yearData = {[district.TimeFrame] : district.Data}
+
+      if (!districtObject[district.Location]) {
+        districtObject[district.Location] = [];
+      } 
+
+      districtObject[district.Location].push(yearData)
+
+      return districtObject
+    }, {})
+
+  return cleanData
+  
+  }
+
 
 }
