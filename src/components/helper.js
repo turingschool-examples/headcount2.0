38cfe1d36a9f43c3bb05f911c9962e2195ae7465
Natalie Montoya <nataliesbarron@gmail.com>
2018-01-22 17:15:26 -0700

    Add a schoolData function to sort out duplicates in data

--- src/components/helper.js
+++ src/components/helper.js
@@ -1,3 +1,15 @@
 export default class DistrictRepository {
+  constructor(data) {
+    this.data = this.schoolData(data)
+  }
 
+  schoolData(info) {
+    return info.reduce((accu, school) => {
+      if(!accu[school.Location]) {
+        accu[school.Location] = {}
+      }
+
+      return accu;
+    }, {})
+  }  
 }
