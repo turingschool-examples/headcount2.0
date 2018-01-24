fd5cff36fec2dd1c1dd00430d02eb9aad19530a4
Jordan Quinn <jpquinn605@gmail.com>
2018-01-24 11:31:46 -0700

    add search bar

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import DistrictRepository from '../../helper.js';
 import kinderData from '../../kindergartners_in_full_day_program.js';
 import CardContainer from '../CardContainer/CardContainer';
+import SearchBar from '../searchbar/SearchBar'
 
 
 const district = new DistrictRepository(kinderData)
@@ -10,13 +11,18 @@ const district = new DistrictRepository(kinderData)
 class App extends Component {
   constructor() {
     super();
-    this.state = {data: district}
+    this.state = {data: district.findAllMatches()}
   }
+
+  // filterCards = (searchValue) => {
+  //   const filteredDistricts = 
+  // }
 
   render() {
     return (
       <div>
         <div>Welcome To Headcount 2.0</div>
+        <SearchBar />
         <CardContainer data={this.state.data}/>
       </div>
     );
