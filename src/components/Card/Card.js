168da9d93e44aeebbbabdc2428b694a28b24e650
parker lindley <fugazi8@gmail.com>
2018-01-24 07:18:51 -0700

    Add greater than class to li's with data larger than 0.5

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -2,12 +2,20 @@ import React from 'react';
 import './Card.css';
 
 const Card = ({ districtName, data, key }) => {
-  const dataValues = Object.keys(data).map( year => year + ': ' + data[year] )
+  const dataValues = Object.keys(data).map( year => {
+  if (data[year] > 0.5) {
+    return <li className="greater-than">{year} + ': ' + {data[year]}</li>
+  } else {
+    return <li>{year} + ': ' + {data[year]}</li>
+  }
+  })
 
   return(
     <article className='Card'>
       <h3>{districtName}</h3>
-      {dataValues.map(value => <p>{value}</p>)}
+      <ul>
+        {dataValues}
+      </ul>
     </article>
   )
 }
