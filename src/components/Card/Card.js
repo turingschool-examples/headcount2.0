4281e9e895055724ea8e7e8c90878c4f79202aeb
Jordan Quinn <jpquinn605@gmail.com>
2018-01-24 10:27:01 -0700

    Render cards with Object.entries

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,14 +1,20 @@
 import React from 'react';
 
-function Card(props) {
+function Card({location, data}) {
+
+  const dataArray = Object.entries(data).map( dataPoint => {
+    return <li> { dataPoint[0] } : { dataPoint[1] } </li>
+  })
+
+
 // turn data props into an array, map over it to plug it into <li>
 // object.value to get the years
 // map over that array and return an array 
 // where each object is an index in the array
-console.log(props.data)
   return (
     <div>
-      <h3> {props.location} </h3>
+      <h3> { location } </h3>
+      <ul> { dataArray } </ul>
     </div> 
   )
 } 
