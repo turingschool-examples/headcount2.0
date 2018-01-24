ab28f8a48abbf85327e34cc4585c51600c66a8d8
Amy Russell <russell.amyh@gmail.com>
2018-01-24 14:06:22 -0700

    Add propTypes

--- src/components/CardContainer/CardContainer.js
+++ src/components/CardContainer/CardContainer.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Card from '../Card/Card';
 import './CardContainer.css';
+import PropTypes from 'prop-types'
 
 function CardContainer(props) {
   let allCities = props.data.map( (city) => {
-    return <Card {...city} />
+    return <Card {...city}/>
   })
 
   return (
@@ -14,5 +15,28 @@ function CardContainer(props) {
 
   )
 }
+
+CardContainer.propTypes = {
+  data: PropTypes.arrayOf(
+    PropTypes.shape({
+      location: PropTypes.string.isRequired,
+
+      data: PropTypes.shape({
+        2004: PropTypes.number.isRequired,
+        2005: PropTypes.number.isRequired,
+        2006: PropTypes.number.isRequired,
+        2007: PropTypes.number.isRequired,
+        2008: PropTypes.number.isRequired,
+        2009: PropTypes.number.isRequired,
+        2010: PropTypes.number.isRequired,
+        2011: PropTypes.number.isRequired,
+        2012: PropTypes.number.isRequired,
+        2013: PropTypes.number.isRequired,
+        2014: PropTypes.number.isRequired
+      }).isRequired
+    })
+  ).isRequired
+};
+
 
 export default CardContainer;
