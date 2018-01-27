c05bf9b730cf90d545a910e9a870c2217692d983
Casey Dallavalle <cbdallavalle@gmail.com>
2018-01-27 13:41:52 -0700

    Add proptypes to CompareInstructions - realized I missed them. Change of Comparison component

--- src/components/CompareInstructions/CompareInstructions.js
+++ src/components/CompareInstructions/CompareInstructions.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Card from '../Card/Card';
 import arrow from '../../images/arrow2.gif';
+import PropTypes from 'prop-types';
 
 
 const CompareInstructions = ({ comparisonData, handleCompareCards }) => {
@@ -16,12 +17,21 @@ const CompareInstructions = ({ comparisonData, handleCompareCards }) => {
         <p className='instructions'>
           click another to compare
         </p>
-        <img src={ arrow } alt="Choose a Card To Compare With" id="arrow" />
+        <img src={arrow} alt="click another card to see a comparison of the two" />
       </div>
       <div className='shadow-card'>
       </div>
      </section>
   )
+}
+
+const { shape, string, objectOf, number, func, object } = PropTypes;
+
+CompareInstructions.propTypes = {
+  handleCompareCards: func.isRequired,
+  comparisonData: shape({
+    comparison: object
+  })
 }
 
 export default CompareInstructions;
--- src/components/Comparison/Comparison.js
+++ src/components/Comparison/Comparison.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Card from '../Card/Card';
+import './Comparison.css';
 
 const Comparison = ({comparisonData, handleCompareCards, removeComparison}) => {
   
