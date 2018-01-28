ee3ed60b30ee7758f86fa42d846aab70ed31cbca
parker lindley <fugazi8@gmail.com>
2018-01-27 19:53:32 -0700

    Add deselect card function

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -9,44 +9,41 @@ it('renders without crashing', () => {
   ReactDOM.render(<App />, div);
 });
 
-it('renders without crashing', () => {
-  shallow(<App />);
-});
-
 it('has initial state with the data and an empty array of selected cards', () => {
-  const wrapper = shallow(<App />)
-  const mockData = {COLORADO: {data:{2004: 0.154, 2005: 0.555}, location: 'COLORADO'}}
-  wrapper.setState({districtData: mockData})
+  const wrapper = shallow(<App />);
+  const mockData = {COLORADO: {data:{2004: 0.154, 2005: 0.555}, location: 'COLORADO'}};
+  
+  wrapper.setState({districtData: mockData});
   expect(wrapper.state().districtData).toEqual(mockData);
   expect(wrapper.state().selectedCards).toEqual([]);
 })
 
 it("should show a card if its state districtData has data in it", () => {
-  const wrapper = shallow(<App />)
-  const mockData = {COLORADO: {data:{2004: 0.154, 2005: 0.555}, location: 'COLORADO'}}
-  wrapper.setState({districtData: mockData})
+  const wrapper = shallow(<App />);
+  const mockData = {COLORADO: {data:{2004: 0.154, 2005: 0.555}, location: 'COLORADO'}};
+
+  wrapper.setState({districtData: mockData});
   expect(wrapper).toMatchSnapshot();
 })
 
 it('should have ONE card in the state selected cards array when a card is selected', () => {
   const wrapper = mount(<App />);
-  const mockData = {COLORADO: {data:{2004: 0.154, 2014: 0.555}, location: 'COLORADO'}, 'ACADEMY 20': {data:{2004: 0.222, 2014: 0.777}, location: 'ACADEMY 20'}}
+  const mockData = {COLORADO: {data:{2004: 0.154, 2014: 0.555}, location: 'COLORADO'}, 'ACADEMY 20': {data:{2004: 0.222, 2014: 0.777}, location: 'ACADEMY 20'}};
   
   wrapper.setState({districtData: mockData});
   expect(wrapper.state().selectedCards).toEqual([]);
   wrapper.find('article').first().simulate('click');
-  expect(wrapper.state().selectedCards.length).toEqual(1)
+  expect(wrapper.state().selectedCards.length).toEqual(1);
 })
 
 it('should have TWO cards in the selectCardArray when two are clicked', () => {
   const wrapper = mount(<App />);
-  const mockData = {COLORADO: {data:{2004: 0.154, 2014: 0.555}, location: 'COLORADO'}, 'ACADEMY 20': {data:{2004: 0.222, 2014: 0.777}, location: 'ACADEMY 20'}}
+  const mockData = {COLORADO: {data:{2004: 0.154, 2014: 0.555}, location: 'COLORADO'}, 'ACADEMY 20': {data:{2004: 0.222, 2014: 0.777}, location: 'ACADEMY 20'}};
   
   wrapper.setState({districtData: mockData});
   wrapper.find('article').first().simulate('click');
   expect(wrapper.state().selectedCards.length).toEqual(1);
   wrapper.find('article').last().simulate('click');
-  console.log(wrapper.state().selectedCards)
   expect(wrapper.state().selectedCards.length).toEqual(2);
 })
 
