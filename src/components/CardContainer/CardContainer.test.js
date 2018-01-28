521c5780057afe0c12a01550e29433373efc7c41
Nyssa Keller <nyssa1@gmail.com>
2018-01-28 08:42:33 -0700

    Add map tests to CardContainer and ComparedCards

--- src/components/CardContainer/CardContainer.test.js
+++ src/components/CardContainer/CardContainer.test.js
@@ -15,4 +15,14 @@ describe('CardContainer', () => {
     expect(wrapper).toMatchSnapshot()
   })
 
+  it('should have a card component for each data set', () => {
+    const mockData = {COLORADO: {data: {2004: .523, 2005: .321}, location: 'COLORADO'}, 'ACADEMY 20': {data: {2004: .283, 2005: .432}, location: 'ACADEMY 20'}}
+    const mockCompare = [{data:{2004: .523, 2005: .321}, location: 'COLORADO'}, {data: {2004: .283, 2005: .432}, location: 'ACADEMY 20'}]
+    const wrapper = shallow(<CardContainer 
+                              districtData={mockData}
+                              comparedCards={mockCompare}
+                              />)
+    expect(wrapper.find('Card').length).toEqual(2)
+  })
+
 })
--- src/components/ComparedCards/ComparedCards.test.js
+++ src/components/ComparedCards/ComparedCards.test.js
@@ -7,9 +7,21 @@ describe('ComparedCards', () => {
 
   it('snapshot test', () => {
     const mockData = [{data: {2004: .523, 2005: .321}, location: 'COLORADO'}, {data: {2004: .283, 2005: .432}, location: 'ACADEMY 20'}]
+    const mockFunction = jest.fn()
     const wrapper = shallow(<ComparedCards 
-                              selectedCards={}/>)
+                              selectedCards={mockData}
+                              makeComparison={mockFunction}/>)
     expect(wrapper).toMatchSnapshot()
+  })
+
+  it('should render two card components if there is two cards in the data', () => {
+    const mockData = [{data: {2004: .523, 2005: .321}, location: 'COLORADO'}, {data: {2004: .283, 2005: .432}, location: 'ACADEMY 20'}]
+    const mockFunction = jest.fn()
+
+    const wrapper = shallow(<ComparedCards 
+                              selectedCards={mockData}
+                              makeComparison={mockFunction}/>)
+    expect(wrapper.find('Card').length).toEqual(2)
   })
 
 })
