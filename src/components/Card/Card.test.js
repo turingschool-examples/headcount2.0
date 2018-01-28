3bee999aeafd85ac5805c293d421bc0f7873e111
Nyssa Keller <nyssa1@gmail.com>
2018-01-28 08:45:06 -0700

    Add map test in Card

--- src/components/Card/Card.test.js
+++ src/components/Card/Card.test.js
@@ -13,4 +13,13 @@ describe('Card', () => {
 
     expect(wrapper).toMatchSnapshot()
   })
+
+  it('should have a li for each data set', () => {
+    const mockData = {2004: 0.24, 2005: 0.278}
+    const mockName = 'Colorado'
+    const wrapper = shallow(<Card 
+                                districtName={mockName}
+                                data={mockData} />)
+    expect(wrapper.find('li').length).toEqual(2)
+  })
 })
