f52423ed346288379908a3876122d29b85594d22
chunktooth <om.saetehn@gmail.com>
2018-03-12 16:30:06 -0600

    Iteration-1-part-1

--- src/helper.js
+++ src/helper.js
@@ -1,14 +1,34 @@
 export default class DistrictRepository {
   constructor(stats) {
-    this.stats = stats.reduce((districts, stat)=> {
-       if (!districts[stat.Location]) {
-        districts[stat.Location] = [];
-       }
-       districts[stat.Location].push({
-        timeFrame: stat.TimeFrame,
-        data: stat.Data
-      })
-       return districts
-    }, {})
+    this.stats = this.cleanData(stats);
+  }
+
+  findByName(name) {
+    if (!name) {
+      return;
+    }
+    const upperCaseName = name.toUpperCase();
+    return this.stats[upperCaseName];
+  }
+
+  cleanData(stats) {
+    return stats.reduce((districts, stat)=> {
+      const name = stat.Location.toUpperCase();
+
+      if (!districts[stat.Location]) {
+        districts[name] = { 
+          location: name,
+          data: {} };
+      }
+      if (stat.Data === 'N/A') {
+        stat.Data = 0;
+      }
+      if (stat.Data < 1 && stat.Data > 0) {
+        stat.Data = parseFloat(stat.Data).toFixed(3);
+      }
+
+      districts[name].data[stat.TimeFrame] = parseFloat(stat.Data);
+      return districts;
+    }, {});
   }
 }
