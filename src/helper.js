2f0d85020509daabaaf501fe0fd824652081a0b5
Amy Russell <russell.amyh@gmail.com>
2018-03-12 17:04:42 -0600

    passing first test and setting up basics of reduce

--- src/helper.js
+++ src/helper.js
@@ -1,5 +1,15 @@
 export default class DistrictRepository {
   constructor(data) {
-    this.stats = {}
+    // console.log(data)
+    this.stats = data.reduce( (districtObj, district) => {
+      if (!districtObj[district[location]]) {
+        districtObj[district[location]] = district
+      }
+       districtObj[district[location]] = district
+     return districtObj
+    }, {})
+  console.log(this.stats)
   }
 }
+
+
