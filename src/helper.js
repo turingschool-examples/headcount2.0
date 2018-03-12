eda879bb0e342a20b2230fb1f30d82be00e11b49
hmmChase <hmmchase@gmail.com>
2018-03-12 15:51:24 -0600

    Initial commit, build data cleaner

--- src/helper.js
+++ src/helper.js
@@ -1,3 +1,26 @@
 export default class DistrictRepository {
+  constructor(data) {
 
+    this.stats = data.reduce((schoolObj, district) => {
+
+      if (!schoolObj[district.Location]) {
+        schoolObj[district.Location];
+      }
+
+      schoolObj[district.Location] = {'data': {[district.TimeFrame]: district.Data}}
+
+
+      if (!schoolObj[district.Location].data['TimeFrame']) {
+        schoolObj[district.Location].data['TimeFrame'] = ''
+      }
+
+
+      // schoolObj[district.Location].data = 
+      
+
+
+      return schoolObj
+
+    }, {})
+  }
 }
