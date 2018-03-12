8f0a2bbf18c98f423f4b10fc38fdf85276eba6ac
Steven Lawson <unluckygamblermusic@gmail.com>
2018-03-12 17:52:08 -0600

    made stats into cleandata method

--- src/helper.js
+++ src/helper.js
@@ -1,15 +1,18 @@
 export default class DistrictRepository {
   constructor(data) {
-    this.stats = data.reduce( (districtObj, district) => {
-      const { Location, TimeFrame, Data } = district;
-      if (!districtObj[Location]) {
-        districtObj[Location] = { location: district.Location,
-                                  timeFrame: district.TimeFrame,
-                                  data: district.Data
-         }
-      }
-     return districtObj
-    }, {})
-  console.log(this.stats)
+  this.stats = this.cleanData(data)
+  }
+  cleanData(data) {
+      return data.reduce( (districtObj, district) => {
+        const { Location, TimeFrame, Data } = district;
+        if (!districtObj[Location]) {
+          districtObj[Location] = { location: district.Location,
+                                    timeFrame: district.TimeFrame,
+                                    data: district.Data
+           }
+        }
+       return districtObj
+      }, {})
+    console.log(this.stats)
   }
 }
