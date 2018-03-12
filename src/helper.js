d8fbdd8d0f752047a9fa969bfc5de3388c2e0602
Steven Lawson <unluckygamblermusic@gmail.com>
2018-03-12 17:30:04 -0600

    passing tests for iteration zero

--- src/helper.js
+++ src/helper.js
@@ -1,15 +1,15 @@
 export default class DistrictRepository {
   constructor(data) {
-    // console.log(data)
     this.stats = data.reduce( (districtObj, district) => {
-      if (!districtObj[district[location]]) {
-        districtObj[district[location]] = district
+      const { Location, TimeFrame, Data } = district;
+      if (!districtObj[Location]) {
+        districtObj[Location] = { location: district.Location,
+                                  timeFrame: district.TimeFrame,
+                                  data: district.Data
+         }
       }
-       districtObj[district[location]] = district
      return districtObj
     }, {})
   console.log(this.stats)
   }
 }
-
-
