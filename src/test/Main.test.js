205d0939e4ea640fed2f30fb5b5d5199bb983cd3
Patrick McLaughlin <patrickmc21@gmail.com>
2018-03-13 14:37:52 -0600

    Add main tests iteration 2

--- src/test/Main.test.js
+++ src/test/Main.test.js
@@ -1,1 +1,19 @@
 import React from 'react';
+import { shallow, mount } from 'enzyme';
+import Main from '../components/Main';
+import DistrictRepository from '../helpers/helper.js';
+import kinderData from '../data/kindergartners_in_full_day_program.js';
+
+describe('Main', () => {
+  it('should match the snapshot', () => {
+    const wrapper = shallow(<Main districts={new DistrictRepository(kinderData)}/>);
+    expect(wrapper).toMatchSnapshot();
+  })
+
+  it('should have props matching district info', () => {
+    const wrapper = mount(<Main districts={new DistrictRepository(kinderData)}/>);
+    const expected = new DistrictRepository(kinderData);
+    expect(wrapper.props().districts).toEqual(expected)
+  })
+  
+})
