8457be60e10281f44ef0a2ce5aeb997aa33f5ce5
hmmChase <hmmchase@gmail.com>
2018-03-13 08:53:34 -0600

    Add findAllMatches method, Update DistrictRepository file path

--- src/helper.js
+++ src/helper.js
@@ -3,28 +3,38 @@ export default class DistrictRepository {
     this.stats = this.newData(data)
   }
 
-  newData(data){
-   return data.reduce((schoolObj, district, index) => {
-      const { TimeFrame, Data, Location } = district 
+  newData(data) {
+    return data.reduce((schoolObj, district, index) => {
+      const { TimeFrame, Data, Location } = district
       const upCaseLocation = Location.toUpperCase()
       const roundedData = Math.round(Data * 1000) / 1000 || 0;
 
-
       if (!schoolObj[upCaseLocation]) {
-        schoolObj[upCaseLocation] = {data: {[TimeFrame]: roundedData}, location: upCaseLocation};
-      } 
+        schoolObj[upCaseLocation] = { data: { [TimeFrame]: roundedData }, location: upCaseLocation };
+      }
       schoolObj[upCaseLocation].data[TimeFrame] = roundedData;
 
       return schoolObj
     }, {})
   }
 
-  findByName(districtName){
-    
+  findByName(districtName) {
     if (districtName) {
       const upperCase = districtName.toUpperCase();
       return this.stats[upperCase];
-    } 
+    }
   }
 
+  findAllMatches(searchString) {
+    const dataLocations = Object.keys(this.stats)
+
+    if (searchString) {
+      const upCaseSearchString = searchString.toUpperCase();
+
+      return dataLocations.filter(location => {
+        return location.startsWith(upCaseSearchString);
+      })
+    }
+    return dataLocations;
+  }
 }
