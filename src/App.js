cc1d8d582abb50bd2fab99db3a2b52b43fc40fbe
Maddy Dannenberg <mmdberg@gmail.com>
2018-03-12 19:36:51 -0600

    Add state to app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
 import React, { Component } from 'react';
-import './App.css';
+import './styles/App.css';
+import DistrictRepository from './helper';
+import kinderData from './data/kindergartners_in_full_day_program.js';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.district = new DistrictRepository(kinderData);
+    this.state={
+      data: {}
+    }
+    console.log(this.district)
+  }
+
+  getData = () => {
+    this.setState({
+      data: this.district.stats
+    })
+  }
+
+  componentDidMount(){
+    this.getData()
+  }
+
   render() {
     return (
       <div>Welcome To Headcount 2.0</div>
