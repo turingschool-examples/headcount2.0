a7415f5b35febc2d1f6e12104f6390a51ccde084
Maddy Dannenberg <mmdberg@gmail.com>
2018-03-13 13:48:24 -0600

    Add visual indication of data above 50 percent

--- src/Card.js
+++ src/Card.js
@@ -2,7 +2,16 @@ import React from 'react';
 import './styles/card.css'
 
 export const Card = ({data, location}) => {
-  const years = Object.keys(data).map(year => <p>{year}: {data[year]}</p>)
+
+  const years = Object.keys(data).map(year => {
+    let roundedPerc = parseFloat(data[year].toFixed(2));
+    if (roundedPerc < 0.5) {
+      return <p>{year}: {roundedPerc}</p>
+    } else {
+      return <p className="goodSchool">{year}: {roundedPerc}</p>
+    }
+      
+    })
   return (
     <div className="card">
       <h2>{location}</h2>
