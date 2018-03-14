686ee8cef65d38e8845c8dd4855cfe741889d055
Michael Gatewood <michael@mngatewood.com>
2018-03-14 17:32:03 -0600

    Adds comparison locations and values

--- src/ComparisonContainer.js
+++ src/ComparisonContainer.js
@@ -5,6 +5,13 @@ import './styles/comparisonContainer.css'
 
 const ComparisonContainer = ({selectedCards, comparison}) => {
   console.log(comparison)
+  let newDistricts = []
+  let newDistrictValues = []
+  if (comparison) {
+    newDistricts = Object.keys(comparison)
+    newDistrictValues = Object.values(comparison)
+    console.log(newDistricts)
+  }
   const comparisonCards = selectedCards.map((district, index) => {
     return (
       <Card location={district.location}
@@ -17,6 +24,14 @@ const ComparisonContainer = ({selectedCards, comparison}) => {
   return(
     <div className="comparison-container">
       {comparisonCards}
+      <div className="comparison-card">
+        <h2>{newDistricts[0]}</h2>
+        <p>{newDistrictValues[0]}</p>
+        <h2>{newDistricts[1]}</h2>
+        <p>{newDistrictValues[1]}</p>
+        <h2>{newDistricts[2]}</h2>
+        <p>{newDistrictValues[2]}</p>
+      </div>
     </div>
   )
 }
