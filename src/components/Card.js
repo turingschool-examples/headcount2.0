bdf5e85f8395c65e8131fde781ccb02fd1df197f
Patrick McLaughlin <patrickmc21@gmail.com>
2018-03-14 11:58:35 -0600

    Add handler for comparison that updates App State

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,14 +2,16 @@ import React from 'react';
 import '../styles/Card.css';
 import PropTypes from 'prop-types'
 
-const Card = ({ info }) => {
+const Card = ({ info, handleSelection }) => {
   const { location, data } = info
   const year = Object.keys(data).map((year, idx) => {
     return data[year] > 0.5 ? <li className="over-fifty" key={idx}>{year} : {data[year]}</li> : <li className="under-fifty" key={idx}>{year} : {data[year]}</li>
   })
 
   return (
-    <article className='card'>
+    <article 
+      className='card'
+      onClick={() => handleSelection(location)}>
       <h2>{location}</h2>
       <ul>
         {year}
