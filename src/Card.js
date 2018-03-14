2ba75b292e7bbf1759debd980f9f7638982ddb12
Jared Eklin <jaredeklin@gmail.com>
2018-03-14 11:32:30 -0600

    Add toggle class on click in Card

--- src/Card.js
+++ src/Card.js
@@ -1,27 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './styles/card.css';
 import PropTypes from 'prop-types'
 
-export const Card = ({data, location}) => {
-  const years = Object.keys(data).map(year => {
-    let roundedPerc = parseFloat(data[year].toFixed(2));
-    if (roundedPerc < 0.5) {
-      return <p key={year}>{year}: {roundedPerc}</p>
-    } else {
-      return <p key={year} className="goodSchool">{year}: {roundedPerc}</p>
-    }
-      
+class Card extends Component {
+  constructor(props) {
+    super(props)
+  }
+  
+  cleanYears = (data) => {
+    return Object.keys(data).map(year => {
+      let roundedPerc = parseFloat(data[year].toFixed(2));
+      if (roundedPerc < 0.5) {
+        return <p key={year}>{year}: {roundedPerc}</p>
+      } else {
+        return <p key={year} className="goodSchool">{year}: {roundedPerc}</p>
+      }       
     })
-  return (
-    <div className="card">
-      <h2>{location}</h2>
-      {years}
-    </div>
-  )
+  }
 
+  handleClick = (event) => {
+    console.log(event.target.closest('div'))
+    event.target.closest('div').classList.toggle('clicked')
+  }
+
+  render(){
+    return (
+      <div className="card" onClick={this.handleClick}>
+        <h2>{this.props.location}</h2>
+        {this.cleanYears(this.props.data)}
+      </div>
+    )
+  }
 }
+
 
 Card.propTypes = {
   data: PropTypes.object.isRequired,
   location: PropTypes.string.isRequired
 }
+
+export default Card
--- src/CardContainer.js
+++ src/CardContainer.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Card} from './Card';
+import Card from './Card';
 import './styles/cardContainer.css';
 import PropTypes from 'prop-types'
 
