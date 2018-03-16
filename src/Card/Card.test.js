c54968bc6b2703850eb7d54b7ed0d6e5465fd774
Sabrina Kennedy <skenne21@gmail.com>
2018-03-16 11:24:24 -0600

    Eslint for Card test

--- src/Card/Card.test.js
+++ src/Card/Card.test.js
@@ -1,12 +1,47 @@
-import React from 'react';
-import ReactDOM from 'react-DOM';
+import React from 'react';  
 import Card from './Card';
 import {shallow} from 'enzyme';
 
 describe('Card', () => {
-	let card, district, stats, selectedLocations;
-	const selectLocation = jest.fn();
-	selectedLocations = [{ 
+  let card, stats, selectedLocations;
+
+  const selectLocation = jest.fn();
+  selectedLocations = [{ 
+    location: 'COLORADO', 
+    stats: {
+      '2017': 0.34,
+      '2016': 0.45
+    } 
+  }, { 
+    location: 'COLORADO SPRINGS', 
+    stats: {
+      '2017': 0.34,
+      '2016': 0.45
+    } 
+  }];
+
+  beforeEach(() => { 
+    stats = { '2017': 0 };
+    card = shallow(<Card 
+      location="COLORADO"
+      stats={stats} 
+      selectLocation={selectLocation} 
+      selectedLocations={selectedLocations} />);
+  });
+  
+  it('should match the snapshot', () => {
+    expect(card).toMatchSnapshot();
+  });
+
+  it('has a class of selected matches in selectedLocations array', () => {
+    selectedLocations = [];
+    card = shallow(<Card 
+      location="COLORADO"
+      stats={stats} 
+      selectLocation={selectLocation} 
+      selectedLocations={selectedLocations} />);
+    expect(card.find('.selected').length).toEqual(0);
+    selectedLocations = [{ 
       location: 'COLORADO', 
       stats: {
         '2017': 0.34,
@@ -19,63 +54,28 @@ describe('Card', () => {
         '2016': 0.45
       } 
     }];
-
-	beforeEach(() => { 
-		stats = { '2017': 0 }
-		card = shallow(<Card 
-			location="COLORADO"
-			stats={stats} 
-			selectLocation={selectLocation} 
-			selectedLocations={selectedLocations} />);
-	});
-	
-	it('should match the snapshot' , () => {
-		expect(card).toMatchSnapshot();
-	});
-
-	it('has a class of selected if the location prop matches a location in the selectedLocations array', () => {
-		selectedLocations = [];
-		card = shallow(<Card 
-			location="COLORADO"
-			stats={stats} 
-			selectLocation={selectLocation} 
-			selectedLocations={selectedLocations} />);
-		expect(card.find('.selected').length).toEqual(0);
-		selectedLocations = [{ 
-      location: 'COLORADO', 
-      stats: {
-        '2017': 0.34,
-        '2016': 0.45
-      } 
-    }, { 
-      location: 'COLORADO SPRINGS', 
-      stats: {
-        '2017': 0.34,
-        '2016': 0.45
-      } 
-    }]
     card = shallow(<Card 
-			location="COLORADO"
-			stats={stats} 
-			selectLocation={selectLocation} 
-			selectedLocations={selectedLocations} />);
+      location="COLORADO"
+      stats={stats} 
+      selectLocation={selectLocation} 
+      selectedLocations={selectedLocations} />);
     expect(card.find('.selected').length).toEqual(1);
-	})
+  });
 
-	it('has a class "red" if district percentile is less than 0.5', () => {
-		expect(card.find('.red').length).toEqual(1);
-		stats['2017'] = 0.51;
-		card = shallow(<Card
-			location="COLORADO"
-			stats={stats} 
-			selectLocation={selectLocation} 
-			selectedLocations={selectedLocations} />);
-		expect(card.find('.red').length).toEqual(0);
-	});
+  it('has a class "red" if district percentile is less than 0.5', () => {
+    expect(card.find('.red').length).toEqual(1);
+    stats['2017'] = 0.51;
+    card = shallow(<Card
+      location="COLORADO"
+      stats={stats} 
+      selectLocation={selectLocation} 
+      selectedLocations={selectedLocations} />);
+    expect(card.find('.red').length).toEqual(0);
+  });
 
-	it('When the article is clicked it should call the selectedLocations function', ()=> {
-		card.find('article').simulate('click');
-		expect(selectLocation).toHaveBeenCalled()
-	})
+  it('When the article is clicked it calls selectedLocations function', ()=> {
+    card.find('article').simulate('click');
+    expect(selectLocation).toHaveBeenCalled();
+  });
 
-})
+});
