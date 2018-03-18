dbabf6ad707d7adde91a7b3d25e48f27a3e7bc62
Matt Walker <mttwlkr@gmail.com>
2018-03-18 13:43:41 -0600

    Add Nav.test lint fixes

--- src/test/Nav.test.js
+++ src/test/Nav.test.js
@@ -1,32 +1,32 @@
 import React from 'react';
-import { shallow, mount } from 'enzyme';
+import { shallow } from 'enzyme';
 import Nav from '../components/Nav';
 
 describe('Nav', () => {
   it('should match the snapshot', () => {
     const wrapper = shallow(<Nav />);
-    expect(wrapper).toMatchSnapshot()
-  })
+    expect(wrapper).toMatchSnapshot();
+  });
 
   it('should have a state of search set to empty string by default', () => {
     const wrapper = shallow(<Nav />);
-    expect(wrapper.state().search).toEqual('')
-  })
+    expect(wrapper.state().search).toEqual('');
+  });
 
   it('should run the search prop method when searchInput is invoked', () => {
     const mockSearch = jest.fn();
     const mockEvent = {target: {value: 'example'}};
-    const wrapper = shallow(<Nav search={mockSearch}/>)
+    const wrapper = shallow(<Nav search={mockSearch}/>);
     wrapper.instance().searchInput(mockEvent);
     expect(mockSearch).toHaveBeenCalled();
-  })
+  });
 
   it('should update state on invoking searchInput', () => {
     const mockSearch = jest.fn();
     const mockEvent = {target: {value: 'example'}};
     const wrapper = shallow(<Nav search={mockSearch}/>);
     wrapper.instance().searchInput(mockEvent);
-    expect(wrapper.state().search).toEqual(mockEvent.target.value)
-    expect(wrapper.state().search).toEqual('example')
-  })
-})
+    expect(wrapper.state().search).toEqual(mockEvent.target.value);
+    expect(wrapper.state().search).toEqual('example');
+  });
+});
