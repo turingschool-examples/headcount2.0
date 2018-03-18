06090db9eb84ac9e37074a5c72703394b8508e51
Matt Walker <mttwlkr@gmail.com>
2018-03-18 13:04:49 -0600

    Fix lint errors in Main and Main.test

--- src/test/Main.test.js
+++ src/test/Main.test.js
@@ -1,17 +1,24 @@
 import React from 'react';
-import { shallow, mount } from 'enzyme';
+import { shallow } from 'enzyme';
 import Main from '../components/Main';
 import DistrictRepository from '../helpers/helper.js';
 import kinderData from '../data/kindergartners_in_full_day_program.js';
 
 describe('Main', () => {
   it('should match the snapshot', () => {
-    const handleComparison = jest.fn()
-    const clearedComparison = jest.fn()
-    const generateComparisons = new DistrictRepository(kinderData).compareDistrictAverages 
+    const handleComparison = jest.fn();
+    const clearedComparison = jest.fn();
+    const generateComparisons = new DistrictRepository(kinderData)
+      .compareDistrictAverages;
     
-    const cards = [{ location: 'Denver', data: {'2006': .098}, selected: true},  
-      { location: 'Golden', data: {'2006': .848}, selected: true}]
+    const cards = [
+      { location: 'Denver', 
+        stats: {'2006': .098}, 
+        selected: true},  
+      { location: 'Golden', 
+        stats: {'2006': .848}, 
+        selected: true}
+    ];
     
     const wrapper = shallow(<Main
       cards={cards}
@@ -19,8 +26,8 @@ describe('Main', () => {
       handleComparison={handleComparison}
       clearedComparison={clearedComparison}
       generateComparisons={generateComparisons}
-      />);
+    />);
       
     expect(wrapper).toMatchSnapshot();
-  })
-})
+  });
+});
