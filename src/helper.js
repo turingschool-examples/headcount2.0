bb36901525051b30cfed707b32f748c169eee64b
Sage Vogt <thurmanvogt@gmail.com>
2018-04-30 16:45:09 -0600

    Add iteration-0 helper functionality

--- src/helper.js
+++ src/helper.js
@@ -1,3 +1,17 @@
 export default class DistrictRepository {
+  constructor(districts) {
+    this.stats = this.organizeData(districts)
+  }
 
+  organizeData(districts) {
+    const districtData = districts.reduce((districtAcc, data) => {
+      if(!districtAcc[data.Location]) {
+        districtAcc[data.Location] = {};
+      }   
+      return districtAcc;
+    }, {});
+    console.log(districtData)
+    return districtData;
+  }
 }
+
