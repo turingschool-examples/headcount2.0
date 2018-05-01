ed339b6e8a57ab233c934de1eabc8f4f3971ef8d
Valentino Valentin <valentinovalentine95@gmail.com>
2018-05-01 16:00:22 -0600

    App test completed

--- src/Components /App/App.js
+++ src/Components /App/App.js
@@ -1,11 +1,24 @@
 import React, { Component } from 'react';
 import './App.css';
-import helper from '../../helper.js';
+import DistrictRepository from '../../helper.js';
+import data from '../../data/kindergartners_in_full_day_program.js';
+import CardContainer from '../CardContainer/CardContainer.js'
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      repo: (new DistrictRepository(data)).stats
+    }
+  }
   render() {
     return (
-      <div>Welcome To Headcount 2.0</div>
+      <div>
+
+        <h1>Welcome To Headcount 2.0</h1>
+        <CardContainer repo={this.state.repo}/>
+
+      </div>
     );
   }
 }
--- src/Components /App/App.test.js
+++ src/Components /App/App.test.js
@@ -3,7 +3,23 @@ import { shallow, mount } from 'enzyme';
 import App from './App';
 import PropTypes from 'prop-types';
 import renderer from 'react-test-renderer';
+import DistrictRepository from '../../helper.js';
+import data from '../../data/kindergartners_in_full_day_program.js';
+import CardContainer from '../CardContainer/CardContainer.js'
 
-it('renders without crashing', () => {
-  shallow(<App />);
-});
+describe('App', () => {
+  it('renders without crashing', () => {
+    shallow(<App />);
+  });
+
+  it('it should have a default state of DistrictRepository', () => {
+    const app = shallow(<App />);
+    expect(app.state('repo')).toEqual((new DistrictRepository(data)).stats)
+  });
+
+  it('should render CardContainer with the correct Props', () => {
+    const app = shallow(<App />);
+    expect(app.find(CardContainer).props()['repo']).toEqual(app.state('repo'))
+  })
+    
+})
