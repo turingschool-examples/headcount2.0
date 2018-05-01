0658dbe1bd88302976c2a0318106e22d76c707db
Seamus Quinn <sumaesquinn@gmail.com>
2018-04-30 18:16:15 -0600

    Pass iteration-0 tests

--- src/helper.js
+++ src/helper.js
@@ -1,3 +1,15 @@
 export default class DistrictRepository {
-  
+  constructor(data) {
+    const schools = data;
+    const cleanData = schools.reduce((acc, schoolObj) => {
+      if (!acc[schoolObj.Location]) {
+        acc[schoolObj.Location] = []
+      }
+
+      acc[schoolObj.Location].push({ year: schoolObj.TimeFrame, data: schoolObj.Data })
+
+      return acc
+    }, {})
+    this.stats = cleanData;
+  }
 }
