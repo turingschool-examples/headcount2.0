9a63792f07dc05a3a382399526983745b42bd34a
meloncatty <k.handel66@gmail.com>
2018-05-01 18:20:53 -0600

    Add tests for app

--- src/App.test.js
+++ src/App.test.js
@@ -1,4 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { shallow, mount } from 'enzyme';
+import CardArea from './CardArea';
+import renderer from 'react-test-renderer';
 import App from './App';
 
+describe('App', () => {
+  it('renders', () => {
+    const app = shallow(<App />);
+
+    expect(app).toHaveLength(1);
+  });
+
+  it('should have default state', () => {
+    const app = shallow(<App />);
+    const expectedState = [];
+
+    expect(app.state('selectedCards')).toEqual(expectedState);
+  });
+
+  it('should render CardArea', () => {
+    const app = shallow(<App />);
+
+    expect(app.find(CardArea)).toHaveLength(1);
+  });
+
+  it('matches the snapsnot', () => {
+    const app = renderer.create(<App />).toJSON();
+
+    expect(app).toMatchSnapshot();
+  })
+});
