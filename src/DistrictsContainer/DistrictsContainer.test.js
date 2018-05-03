05fcb6e53c080bbff4dc741adb082ab050fec31f
Sage Vogt <thurmanvogt@gmail.com>
2018-05-02 18:25:04 -0600

    Add snapshot testing to DistrictsContainer test

--- src/DistrictsContainer/DistrictsContainer.test.js
+++ src/DistrictsContainer/DistrictsContainer.test.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import DistrictsContainer from './DistrictsContainer';
 import { shallow, mount } from 'enzyme';
-import '../setupTests';
 import DistrictCard from '../DistrictCard/DistrictCard';
+import renderer from 'react-test-renderer';
+import '../setupTests';
 
 describe('<DistrictsContainer />', () => {
   
-  it('renders a `.district-container`', () => {
-    const expected = {
+  it('renders correctly', () => {
+    const districts = {
       'COLORADO': {
         location: 'COLORADO',
         data: {},
@@ -16,10 +17,27 @@ describe('<DistrictsContainer />', () => {
         location: 'COLORADO SPRINGS',
         data: {}
     }}
-    const wrapper = shallow(<DistrictsContainer districts={expected}/>);
+    const actual = renderer
+      .create(<DistrictsContainer districts={districts}/>)
+      .toJSON();
 
-    expect(wrapper.find('.district-container')).toHaveLength(1);
+    expect(actual).toMatchSnapshot();
   });
+
+  // it('renders a `.district-container`', () => {
+  //   const expected = {
+  //     'COLORADO': {
+  //       location: 'COLORADO',
+  //       data: {},
+  //     }, 
+  //     'CSPRINGS': {
+  //       location: 'COLORADO SPRINGS',
+  //       data: {}
+  //   }}
+  //   const wrapper = shallow(<DistrictsContainer districts={expected}/>);
+
+  //   expect(wrapper.find('.district-container')).toHaveLength(1);
+  // });
 
   it('renders children for as many DistrictCards that are passed in', () => {
     const expected = {
