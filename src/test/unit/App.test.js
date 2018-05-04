cd24b5ecaf04b968e0790165fc028d3b37476ab1
tmcjunkinmarquis <tmcjunkinmarquis@gmail.com>
2018-05-03 19:07:34 -0600

    Test App.js

--- src/test/unit/App.test.js
+++ src/test/unit/App.test.js
@@ -1,37 +1,46 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from '../../App';
-import {shallow, mount } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 // import renderer from 'react-test-renderer';
 import CardContainer from '../../CardContainer'
 
-describe ('App',()=>{
+describe('App', () => {
   let app;
 
-  beforeEach(()=>{
+  beforeEach(() => {
     app = shallow(<App />)
   })
 
-  it('matches snapshot', () => {
-    
+  it.skip('matches snapshot', () => {
     expect(app).toMatchSnapshot()
   });
 
-  it('state is equal to an object',()=>{
-
-    expect(app.state().schools).toBeInstanceOf(Object)
+  it('state is equal to an object', () => {
+    expect(app.state().schoolData).toBeInstanceOf(Object)
   })
 
-  it('should have state.data with length of 181',()=>{
-
-  expect(app.state().data).toHaveLength(181)
+  it('should have state.data with length of 181', () => {
+    expect(app.state().schoolNames).toHaveLength(181)
   })
 
-  it('renders CardContainer with correct props',()=>{
-    
-    expect(app.find(CardContainer).prop()).toEqual({})
+  it('renders CardContainer with correct props', () => {
+    expect(app.find(CardContainer).prop('schoolData')).toBeInstanceOf(Object)
   })
 
+  it('initiates having state with correct properties', () => {
+    const actual = Object.keys(app.instance().state)
+    const expected = ['schoolRepository', 'schoolNames', 'schoolData']
 
+    expect(actual).toEqual(expected)
+  })
+
+  it('calls componentDidMount', () => {
+    const spy = jest.spyOn(App.prototype, 'componentDidMount');
+    const app = mount(<App />);
+    app.instance().componentDidMount
+
+    expect(spy).toHaveBeenCalled()
+  })
 })
 
