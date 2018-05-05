4df72937b7324027ae8bb04c25a78ed8ff8e2062
Seamus Quinn <sumaesquinn@gmail.com>
2018-05-05 12:09:38 -0600

    Create updateComparedSchools method

--- src/CardContainer.js
+++ src/CardContainer.js
@@ -2,11 +2,15 @@ import React from 'react';
 import Card from './Card';
 import './styles/CardContainer.css';
 
-const CardContainer = ({ schoolData, schoolNames }) => {
+const CardContainer = ({ schoolData, schoolNames, updateComparedSchools }) => {
 
   const cards = schoolNames.map((schoolName, index) => {
     const cardData = schoolData[schoolName];
-    return <Card cardData={cardData} key={index} />;
+    return <Card 
+    cardData={cardData} 
+    key={index} 
+    updateComparedSchools={updateComparedSchools}
+    />;
   });
   return cards;
 };
