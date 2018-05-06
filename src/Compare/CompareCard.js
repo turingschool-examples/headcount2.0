7e1b78723fae38588a96a3b12ce34bf14d736646
Eric Mellow <ericmellow@gmail.com>
2018-05-05 19:32:42 -0600

    Add CSS styling

--- src/Compare/CompareCard.js
+++ src/Compare/CompareCard.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import './Compare.css';
+import './ComparedCard.css';
 
 const CompareCard = ({compareInfo}) => {
   const newCompareInfo = Object.keys(compareInfo)
   return (
-    <div className="card" onClick={this.handleCardClick}>
+    <div className="card clicked compare-card" onClick={this.handleCardClick}>
       <h3>
         {`${newCompareInfo[0]}: ${compareInfo[newCompareInfo[0]]}`}
       </h3>
       <h3>
-        {`<-------${compareInfo.compared}------->`}
+        {`<------ ${compareInfo.compared} ------>`}
       </h3>
       <h3>
         {`${newCompareInfo[1]}: ${compareInfo[newCompareInfo[1]]}`}
