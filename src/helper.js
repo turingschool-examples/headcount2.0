f7001526446755e1c9d710630fa665eb9ed525a7
Tory Dannenberg <rfdberg@gmail.com>
2018-06-25 15:47:45 -0600

    Finish reduce method to eliminate duplicate locations

--- src/helper.js
+++ src/helper.js
@@ -1,5 +1,14 @@
+import kinderData from './data/kindergartners_in_full_day_program.js'
+
 export default class DistrictRepository {
-  stats = {
-      location: 'Denver'
-    }
+  constructor() {
+    this.stats = kinderData.reduce((stats, stat) => {
+      let statObj = { [stat.TimeFrame] : stat.Data }
+        if (!stats[stat.Location]) {
+          stats[stat.Location] = statObj
+        } 
+      stats[stat.Location] = { ...statObj, ...stats[stat.Location]}
+      return stats
+      }, {})
+  }
   }
