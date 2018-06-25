6bd88cee16fd251819ce9e95f7e9be0d1c71f4b8
Joel O'Connor <joconno07@gmail.com>
2018-06-25 15:03:23 -0600

    Add in removeDuplicate methods and passed iteration-0 tests

--- src/helper.js
+++ src/helper.js
@@ -1,7 +1,19 @@
 import kindergartnerData from './data/kindergartners_in_full_day_program';
 
 export default class DistrictRepository {
-  constructor () {
-    this.stats = [...kindergartnerData];
+  constructor() {
+    this.stats = this.removeDuplicates(kindergartnerData);
+  }
+
+  removeDuplicates(initialData) {
+    const noDuplicateSchools = initialData.reduce((arrangedSchools, school) => {
+      if (!arrangedSchools[school.Location]) {
+        arrangedSchools[school.Location] = [school];
+      } else {
+        arrangedSchools[school.Location] = [...arrangedSchools[school.Location], school];
+      }
+        return arrangedSchools;
+    }, {});
+    return noDuplicateSchools;
   }
 }
