cd5d82d0622339c3385d54df1689d7e291028de8
Quin Hill <quinhill@icloud.com>
2018-06-25 14:11:31 -0600

    Set up cleanData function in helper.js

--- src/helper.js
+++ src/helper.js
@@ -1,3 +1,22 @@
 export default class DistrictRepository {
+  constructor(kinderData) {
+    this.stats = this.cleanedData(kinderData)
+  }
 
+  cleanedData = (kinderData) => {
+    return kinderData.reduce((cleanedKinder, school) => {
+      const upperCaseDistrict = school.Location.toUpperCase()
+
+      if (!cleanedKinder[upperCaseDistrict]) {
+        cleanedKinder[upperCaseDistrict] = {
+          location: upperCaseDistrict,
+          stats: {},
+          selected: false
+        }
+      }
+
+      cleanedKinder[school] = {...school}
+      return cleanedKinder
+    }, {})
+  }
 }
