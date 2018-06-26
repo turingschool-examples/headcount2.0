32072ce3d4cd32e8d31e86ed50f74be6d3fd73fa
Jeremiah Stanley <jeremiah.j.stanley@gmail.com>
2018-06-26 15:16:00 -0600

    Build out stateless DistrictCard component

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,10 +1,23 @@
 import React, { Component } from 'react';
 import './App.css';
+import ControlledForm from '../ControlledForm/ControlledForm'
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      matchingDistricts: [],
+    }
+  }
+
+
+
   render() {
     return (
-      <div>Welcome To Headcount 2.0</div>
+      <div>
+        <h1>Count dem Heads</h1>
+        <ControlledForm />
+      </div>
     );
   }
 }
