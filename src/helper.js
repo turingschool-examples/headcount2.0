a4979eed1eef9896603827e4c04eda1fce7f0766
Lee Graham <graham.la3@gmail.com>
2018-06-26 16:16:22 -0600

    Struggle to pass the third test without failing the first test, study error messages for solution

--- src/helper.js
+++ src/helper.js
@@ -3,7 +3,7 @@ import kinderData from './data/kindergartners_in_full_day_program.js'
 export default class DistrictRepository {
   constructor() {
     this.stats = kinderData.reduce((stats, stat) => {
-      let statObj = { [stat.TimeFrame] : stat.Data }
+      const statObj = { [stat.TimeFrame] : stat.Data }
         if (!stats[stat.Location]) {
           stats[stat.Location] = statObj
         } 
@@ -12,22 +12,33 @@ export default class DistrictRepository {
       }, {})
   }
 
-  findByName = (data) => {
-    const items = kinderData.reduce((items, item) => {
-      if (item.Location === data){
+  findByName = (search) => {
+      // if (!search) {
+      //   return 
+      // } 
 
-      const location = item.Location
-      items[location] = data
+    let stats = kinderData.reduce((stats, stat) => {
+      const statObj = { [stat.TimeFrame] : stat.Data }
+        if (!stats[stat.Location]) {
+          stats[stat.Location] = statObj
+        } 
+      stats[stat.Location] = { ...statObj, ...stats[stat.Location]}
+      return stats
+      }, {})
+
+    const items = Object.keys(stats).reduce((items, stat) => {
+      if(stat === search) {
+        items['location'] = stat.toUpperCase()
       }
+        // console.log(items.location )
       return items
-      console.log(items)
-
-    }, {})
-    return items
+    }, {}) 
   }
 
+
+
   findAllMatching = (data) => {
-    const matches = data.filter(match => match.stuff)
+    const matches = kinderData.map(stat => stat.stuff)
 
   }
 }
