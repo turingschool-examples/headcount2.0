b00159d4c36e44213c5d35cb342aee727c9fffaf
Joel O'Connor <joconno07@gmail.com>
2018-06-25 22:09:43 -0600

    Added in findByName method functionality, partially passing 3rd test on iteration1-part1 test

--- src/helper.js
+++ src/helper.js
@@ -1,19 +1,30 @@
-import kindergartnerData from './data/kindergartners_in_full_day_program';
-
 export default class DistrictRepository {
-  constructor() {
+  constructor(kindergartnerData) {
     this.stats = this.removeDuplicates(kindergartnerData);
   }
 
   removeDuplicates(initialData) {
     const noDuplicateSchools = initialData.reduce((arrangedSchools, school) => {
-      if (!arrangedSchools[school.Location]) {
-        arrangedSchools[school.Location] = [school];
+      const sanitizedLocation = school.Location.toUpperCase();
+
+      if (!arrangedSchools[sanitizedLocation]) {
+        arrangedSchools[sanitizedLocation] = [school];
       } else {
-        arrangedSchools[school.Location] = [...arrangedSchools[school.Location], school];
+        arrangedSchools[sanitizedLocation] = [...arrangedSchools[sanitizedLocation], school];
       }
         return arrangedSchools;
     }, {});
     return noDuplicateSchools;
+  }
+
+  findByName(searchCriteria) {
+    if (searchCriteria) {
+      const upperCaseSearchCriteria = searchCriteria.toUpperCase();
+      if (this.stats[upperCaseSearchCriteria]) {
+        return {
+          location: this.stats[upperCaseSearchCriteria]
+        };
+      }
+    }
   }
 }
