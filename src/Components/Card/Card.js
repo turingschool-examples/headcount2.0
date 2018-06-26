2c7f701d1e558f1cd3cbfefab0026fc7fdceccc7
Alan Charles <alanjcharles14@gmail.com>
2018-06-26 16:24:18 -0600

    Add final styling for cards

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -6,8 +6,10 @@ const Card = (props) => {
 
   const spreadStats =
     Object.keys(stats).map((stat, index) => {
+      const statLevel = stats[stat] > .5 ? 'high-stat' : 'low-stat';
+
       return (
-        <div className="stat-container">
+        <div className="stat-container" id={statLevel}>
           <h5 className="year">{stat}:</h5>
           <p className="stat">{stats[stat]}</p>
         </div>
@@ -17,9 +19,7 @@ const Card = (props) => {
 
   return (
     <div className="card">
-     
       <h1 className="location">{location}</h1>
-
       <div>
       {spreadStats}
       </div>
