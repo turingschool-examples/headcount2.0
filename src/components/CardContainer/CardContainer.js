cfec55da5d50572a00599c7b353b0c4b0ced6fe0
Joel O'Connor <joconno07@gmail.com>
2018-06-27 14:57:09 -0600

    CardContainer now populates Card components, Card componets now populate data

--- src/components/CardContainer/CardContainer.js
+++ src/components/CardContainer/CardContainer.js
@@ -3,9 +3,14 @@ import PropTypes from 'prop-types';
 import Card from '../Card/Card';
 
 const CardContainer = props => {
+  const { districts } = props;
+
+  const districtData = districts.map(district => <Card {...district} key={district.location} />)
+
   return (
     <div>
       <h1>Card Container</h1>
+      {districtData}
     </div>
   );
 };
