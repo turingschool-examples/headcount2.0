34ebe23e16052ff4d527da1dde7dfa7f805968f9
Tory Dannenberg <rfdberg@gmail.com>
2018-06-27 12:58:44 -0600

    Write tests for Search Form component

--- src/SearchForm.test.js
+++ src/SearchForm.test.js
@@ -3,11 +3,40 @@ import React from 'react';
 import { shallow } from 'enzyme';
 
 describe('SearchForm Tests', () => {
-  it('should update state when the input value changes', () => {
+  it('should invoke handleInput when the input value changes', () => {
+    const mockInput = jest.fn()
+    const wrapper = shallow(<SearchForm 
+                              type="text"
+                              placeholder="Enter your district"
+                              value={jest.fn()}
+                              onChange={mockInput}
+                              onSubmit={jest.fn()}/>)
     
+    wrapper.find('input').simulate('keydown', {target: {value: 'abc'}})
+    expect(mockInput).toHaveBeenCalled()
   })
 
+  it('should update state when handle input is invoked', () => {
+    const wrapper = shallow(<SearchForm />);
+    const mockTyping = { target: {value: 'taco'}};
+    const expected = 'taco';
 
+    wrapper.instance().handleInput(mockTyping);
 
+    expect(wrapper.state('district')).toEqual(expected)
 
+  })
+
+  it('should invoke updateDistricts when the submit button is clicked', () => {
+    const mockUpdateDistricts = jest.fn()
+    const wrapper = shallow(<SearchForm
+      type="text"
+      placeholder="Enter your district"
+      value={jest.fn()}
+      onChange={jest.fn()}
+      onSubmit={mockUpdateDistricts} />)
+
+    wrapper.find('button').simulate('click')
+    expect(mockUpdateDistricts).toHaveBeenCalled()
+  })
 })
