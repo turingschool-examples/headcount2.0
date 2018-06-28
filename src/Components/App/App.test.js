8c4e568daad12a6a21aead31a55c6820175659c5
Jeremiah Stanley <jeremiah.j.stanley@gmail.com>
2018-06-28 14:58:08 -0600

    Add proptypes and snapshot test to controlledform component

--- src/Components/App/App.test.js
+++ src/Components/App/App.test.js
@@ -2,7 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  // ReactDOM.render(<App />, div);
-});
+
+describe('App', () => {
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    // ReactDOM.render(<App />, div);
+  });
+
+  it('should match the snapshot', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('should update state after componentDidMount runs', () => {
+    const initialState = {};
+    
+ })
+  it('findDistrict function should filter the state', () => {
+
+  })
+
+})
+
