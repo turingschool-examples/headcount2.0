15e8e3ef924ac9f78357b88c550a90f594e7795d
Tory Dannenberg <rfdberg@gmail.com>
2018-06-28 18:50:00 -0600

    Improve CardContainer testing to 100 percent coverage

--- src/CardContainer.test.js
+++ src/CardContainer.test.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import CardContainer from './CardContainer.js';
 import { shallow } from 'enzyme';
+import Card from './Card.js'
 
 describe('Card Container', () => {
-  it('should render main tags to the app', () => {
-    // setup
-    const wrapper = shallow(<CardContainer />)
+  it('should match the snapshot', () => {
+    const mockCards = { 'ACADEMY 20': { 2004: 0.75 }, 'TacoLand': { 2004: 0.75 } }
+    const wrapper = shallow(<CardContainer cards={mockCards} />)
+    expect(wrapper).toMatchSnapshot()
+  })
 
-    // expectation
-    const expected = <main></main>
-    expect(wrapper.contains(expected)).toEqual(true)
+  it('should render all of the districts', () => {
+    const mockCards = { 'ACADEMY 20': { 2004: 0.75 }, 'TacoLand': { 2004: 0.75 }}
+    const wrapper = shallow(<CardContainer cards={mockCards}/>)
+
+  
+    const expected = wrapper.find(Card).length
+    expect(expected).toEqual(2)
   })
 
 })
