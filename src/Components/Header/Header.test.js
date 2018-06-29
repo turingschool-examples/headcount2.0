95b67e0f3f4f4694ed06cf4420c2175facae47c0
Alan Charles <alanjcharles14@gmail.com>
2018-06-29 12:46:49 -0600

    Add app test and header test

---
+++ src/Components/Header/Header.test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Header from './Header'
+
+describe('Header', () => {
+ let wrapper = shallow(<Header />)
+
+ it('should match the snapshot', () => {
+
+    expect(wrapper).toMatchSnapshot()
+ })
+});
