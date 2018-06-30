77d30d4b15cce61dab10f341757235e21f237581
Marika Ross <marika.ross@gmail.com>
2018-06-30 15:48:10 -0600

    Outline 2 tests for App file and began rudimentary css

--- src/Components/App/App.test.js
+++ src/Components/App/App.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {shallow} from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import App from './App';
 
 
@@ -15,13 +15,72 @@ describe('App', () => {
     expect(wrapper).toMatchSnapshot();
   });
 
-  it('should update state.matchingDistricts after componentDidMount runs', () => {
+  it.skip('should update state.matchingDistricts after componentDidMount runs', () => {
+    const wrapper = shallow(<App />);
     const initialState = {};
+    const mockMount = jest.fn()
+    const mockState = { COLORADO: 
+                             { stats: 
+                                { 2004: 0.24,
+                                  2005: 0.278,
+                                  2006: 0.337,
+                                  2007: 0.395,
+                                  2008: 0.536,
+                                  2009: 0.598,
+                                  2010: 0.64,
+                                  2011: 0.672,
+                                  2012: 0.695,
+                                  2013: 0.703,
+                                  2014: 0.741 },
+                               location: 'COLORADO' }
+                              }
+    // wrapper.instance().mockMount;
+    wrapper.setState({ mockState })
+    const expectedState = { COLORADO: 
+                             { stats: 
+                                { 2004: 0.24,
+                                  2005: 0.278,
+                                  2006: 0.337,
+                                  2007: 0.395,
+                                  2008: 0.536,
+                                  2009: 0.598,
+                                  2010: 0.64,
+                                  2011: 0.672,
+                                  2012: 0.695,
+                                  2013: 0.703,
+                                  2014: 0.741 },
+                               location: 'COLORADO' }
+                              }
+  const actualState = wrapper.instance().state
+  expect(actualState).toEqual(expectedState);
     
  })
-  it('findDistrict function should filter the state.matchingDistricts', () => {
+  it.only('findDistrict function should be called with the correct props', () => {
+    const wrapper = shallow(<App />);
+    const findDistrictMock = jest.fn();
+    const mockInput = 'Colorado';
+    const mockEvent = { event: mockInput }
+    console.log(findDistrictMock)
+    wrapper.instance().findDistrictMock()
 
+    expect(findDistrictMock).toBeCalledWith(mockInput);
+  
   })
 
+//   describe('<Gallery />', () => {
+//   it('Expect sendMessageToParentWindow to be called on image change', () => {
+//     const sendEventToParentWindowMock = jest.fn();
+//     const onChangeImageMock = jest.fn(() => {
+//          sendEventToParentWindowMock();
+//     });
+
+//     const gallery = shallow(<Gallery images={imagesMockData} onChange={onChangeImageMock} />); // Passing the mocked onChangeImage as prop
+//     gallery.find('input#image-1').simulate('change');
+
+//     expect(sendEventToParentWindowMock).toBeCalled();
+//   });
+// }
+
 })
+
 
