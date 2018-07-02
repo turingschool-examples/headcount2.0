8709f7a7ee32063126c7be3334f7a5ddcead320a
Jeremiah Stanley <jeremiah.j.stanley@gmail.com>
2018-07-02 10:53:52 -0600

    Add proptypes to components

--- src/Components/App/App.test.js
+++ src/Components/App/App.test.js
@@ -44,9 +44,10 @@ describe('App', () => {
 
   it('selectDictrict removes a district from the state when the card is clicked again', () => {
     const wrapper = shallow(<App />);
-    const mockInput = 'ACADEMY 20';
-    const initialState = { COLORADO: 
-                           { stats: 
+    const mockInput = 'COLORADO';
+    const initialState = { COLORADO:
+                           { selected: true, 
+                             stats: 
                               { 2004: 0.24,
                                 2005: 0.278,
                                 2006: 0.337,
@@ -58,10 +59,10 @@ describe('App', () => {
                                 2012: 0.695,
                                 2013: 0.703,
                                 2014: 0.741 },
-                           location: 'COLORADO',
-                           selected: true },
-                          'ACADEMY 20': 
-                           { stats: 
+                           location: 'COLORADO'},
+                          'ACADEMY 20':  
+                           { selected: true,
+                             stats: 
                               { 2004: 0.302,
                                 2005: 0.267,
                                 2006: 0.354,
@@ -73,27 +74,28 @@ describe('App', () => {
                                 2012: 0.479,
                                 2013: 0.488,
                                 2014: 0.49 },
-                           location: 'ACADEMY 20',
-                           selected: true} }
-    const expectedState = { COLORADO: 
-                             { stats: 
-                                { 2004: 0.24,
-                                  2005: 0.278,
-                                  2006: 0.337,
-                                  2007: 0.395,
-                                  2008: 0.536,
-                                  2009: 0.598,
-                                  2010: 0.64,
-                                  2011: 0.672,
-                                  2012: 0.695,
-                                  2013: 0.703,
-                                  2014: 0.741 },
-                             location: 'COLORADO',
-                             selected: true }
+                           location: 'ACADEMY 20'}
+                          }
+    const expectedState = {'ACADEMY 20':  
+                           { selected: true,
+                             stats: 
+                              { 2004: 0.302,
+                                2005: 0.267,
+                                2006: 0.354,
+                                2007: 0.392,
+                                2008: 0.385,
+                                2009: 0.39,
+                                2010: 0.436,
+                                2011: 0.489,
+                                2012: 0.479,
+                                2013: 0.488,
+                                2014: 0.49 },
+                           location: 'ACADEMY 20'}
                           }
     wrapper.setState({comparedDistricts: initialState});
     wrapper.instance().selectDistrict(mockInput);
-    expect(initialState).toEqual(expectedState);
+    const actualState = wrapper.state().comparedDistricts
+    expect(expectedState).toBe(actualState);
   });
 
 });
