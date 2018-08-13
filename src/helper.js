2518812920eba542728aedd6d112bf5a35ee5a17
Chris Boylen <billpolster@gmail.com>
2018-08-13 15:15:09 -0600

    Create method on helper component to filter data without duplicates

--- src/helper.js
+++ src/helper.js
@@ -1,3 +1,19 @@
-export default class DistrictRepository {
+import React from 'react';
+import kinderData from './data/kindergartners_in_full_day_program.js';
 
+
+export default class DistrictRepository {
+  constructor() {
+    this.stats = this.filterData(kinderData)
+  }
+
+  filterData = (data) => {
+    return data.reduce((acc, location) => {
+      if (!acc[location.Location]) {
+        acc[location.Location] = []
+      }
+      acc[location.Location].push(location)
+      return acc
+    }, {})
+  }
 }
