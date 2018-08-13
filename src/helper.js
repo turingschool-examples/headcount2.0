d643765a6e6939ba7747c01807618537143c279b
Jesse McFadden <jessemcbrennan@gmail.com>
2018-08-13 15:41:24 -0600

    Add working removeDuplicates method to DistrictRepository

--- src/helper.js
+++ src/helper.js
@@ -1,3 +1,23 @@
-export default class DistrictRepository {
+import React from 'react'
+import kinderData from './data/kindergartners_in_full_day_program.js';
 
+export default class DistrictRepository {
+  constructor(stats) {
+    this.stats = this.removeDuplicates(kinderData)
+  }
+
+  removeDuplicates = (data) => {
+    return data.reduce((acc, location) => {
+        if(!acc[location.Location]) {
+          acc[location.Location] = []
+        }
+        acc[location.Location].push(location)
+  
+        return acc
+
+    },{})
+  }
 }
+
+
+
