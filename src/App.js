3f795baa7748024116d778df34fdb6915f732fc4
John DoeMeganHaubelt <meganhaubelt@gmail.com>
2018-08-13 15:41:34 -0600

    Iteration 0 tests are passing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import React, { Component } from 'react';
+import DistrictRepository from '../../helper.js';
 import './App.css';
 
 class App extends Component {
+	constructor() {
+		super()
+	}
   render() {
     return (
       <div>Welcome To Headcount 2.0</div>
--- src/helper.js
+++ src/helper.js
@@ -1,3 +1,27 @@
+import kinderData from './data/kindergartners_in_full_day_program.js';
+
+
 export default class DistrictRepository {
+	constructor(stats) {
+		this.stats = this.removeDuplicate(kinderData)
+		
+	}
+
+	removeDuplicate = (data) => {
+		return data.reduce((acc, school) => {
+			if(!acc[school.Location]) {
+				acc[school.Location] = [] 
+			}
+			acc[school.Location].push(school)
+
+			return acc;
+
+		}, {});
+	}
+
+	findByName = () => {
+
+	}
+
 
 }
