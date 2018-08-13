f5ad90226461515b4e845a630bbcd935f6f6c6c0
Laura Whitaker <laura.k.whitaker@gmail.com>
2018-08-13 15:19:59 -0600

    Remove duplicate data

--- src/helper.js
+++ src/helper.js
@@ -1,3 +1,24 @@
-export default class DistrictRepository {
+import { access } from "fs";
 
+export default class DistrictRepository {
+    constructor(data) {
+        this.stats = this.removeDuplicates(data);
+    }
+
+    removeDuplicates = (data) => {
+        return data.reduce((dataLocationObj, dataObj) => {
+            let locationKey = dataLocationObj[dataObj.Location];
+            const relevantData = {
+                "TimeFrame": dataObj.TimeFrame,
+                "DataFormat": dataObj.DataFormat,
+                "Data": dataObj.Data
+            };
+
+            if (!locationKey) {
+                locationKey = [];
+            }
+            dataLocationObj[dataObj.Location] = [...locationKey, relevantData];
+            return dataLocationObj;
+        }, {})
+    }
 }
