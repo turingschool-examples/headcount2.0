3014e3bd74d739691a448a501be26c5ea72f9910
Joel O'Connor <joconno07@gmail.com>
2018-08-13 17:50:44 -0600

    initial data clean

--- src/helper.js
+++ src/helper.js
@@ -1,3 +1,20 @@
 export default class DistrictRepository {
+  constructor(schoolData) {
+    this.stats = this.districtDataCleaner(schoolData);
+  }
 
+  districtDataCleaner(intitialData) {
+    return intitialData.reduce((organizedData, school) => {
+      if (!organizedData[school.Location]) {
+        organizedData[school.Location] = [school];
+      } else {
+        organizedData[school.Location] = [school, ...organizedData[school.Location]];
+      }
+      return organizedData;
+    }, {});
+  }
+
+  findByName(userInput) {
+    return this.stats[userInput];
+  }
 }
