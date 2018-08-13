da0ca69d6b592eba08f0ed0d42a0853b424e3c64
Gray Smith <gray.smith00@gmail.com>
2018-08-13 17:15:49 -0600

    Fix iteration 0 code

--- src/helper.js
+++ src/helper.js
@@ -2,18 +2,28 @@
 
 export default class DistrictRepository {
   constructor(stats) {
-    this.stats = stats;
+    this.stats = this.removeDuplicates(stats);
   }
 
   removeDuplicates = array => {
-    const noDuplicates = array.reduce((noDuplicates, stat) => {
+    return array.reduce((noDuplicates, stat) => {
       const { Location } = stat;
       if (!noDuplicates[Location]) {
         noDuplicates[Location] = stat;
       }
       return noDuplicates;
     }, {});
+  };
 
-    this.stats = noDuplicates;
+  findByName = name => {
+    // const foundStat = Object.keys(this.stats).find(statKey => {
+    //   return statKey === name;
+    // });
+    // return foundStat;
+    //   if (!name) return undefined;
+    //   const foundLocation = this.stats.find(stat => {
+    //     return stat.Location.toLowerCase() === name.toLowerCase();
+    //   });
+    //   return foundLocation;
   };
 }
