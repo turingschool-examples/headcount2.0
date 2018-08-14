c994ed3079ebf433d56fc562385aaa5d579a1934
John DoeMeganHaubelt <meganhaubelt@gmail.com>
2018-08-13 18:40:12 -0600

    Pass iteration1 tests through 5 tests

--- src/helper.js
+++ src/helper.js
@@ -3,24 +3,46 @@ import kinderData from './data/kindergartners_in_full_day_program.js';
 
 export default class DistrictRepository {
 	constructor(stats) {
-		this.stats = this.removeDuplicate(kinderData)
+		this.stats = this.removeDuplicates(kinderData)
 		
 	}
 
-	removeDuplicate = (data) => {
-		return data.reduce((acc, school) => {
-			if(!acc[school.Location]) {
-				acc[school.Location] = [] 
-			}
-			acc[school.Location].push(school)
-
+	removeDuplicates = (kinderData) => {
+		return kinderData.reduce((acc, school) => {
+			let location = school.Location.toUpperCase();
+			let roundedData= Math.round(school.Data * 1000) / 1000
+			if(!acc[location]) {
+				acc[location] = {location, stats: {school.TimeFrame = roundedData} 
+			// acc[school.Location]= location
+		}}
 			return acc;
 
 		}, {});
+
 	}
 
-	findByName = () => {
+	findByName = (districtName = '') => {
+		console.log(this.stats)
+		const upperDistrict = districtName.toUpperCase();
+		
+		if(!this.stats[upperDistrict]) {
+			return undefined} 
 
+		// console.log(this.stats)
+		if (this.stats[upperDistrict]) {
+			return this.stats[upperDistrict]
+		}
+		
+	
+		
+		
+
+	}
+
+	findAllMatches = (districtName) => {
+		return kinderData.reduce((acc, school) => {
+
+		}, [])
 	}
 
 
