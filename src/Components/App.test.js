9b2429e1a9708252f0d0196ed95fcd6f933a049e
Chris Boylen <billpolster@gmail.com>
2018-08-14 08:15:19 -0600

    Add and pass test so that app has default state

--- src/Components/App.test.js
+++ src/Components/App.test.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import App from './App';
+import DistrictRepository from '../helper';
+import kinderData from '../data/kindergartners_in_full_day_program';
 
 describe('APP', () => {
   let wrapper;
@@ -11,5 +13,14 @@ describe('APP', () => {
 
   it('Should exist', () => {
     expect(wrapper).toBeDefined();
+  })
+
+  it('Should have a default state of data with districtRepo.stats and selectedCards with an empty array', () => {
+    const districtRepo = new DistrictRepository(kinderData);
+
+    expect(wrapper.state()).toMatchObject({
+      data: districtRepo.stats,
+      selectedCards: []
+    });
   })
 })
