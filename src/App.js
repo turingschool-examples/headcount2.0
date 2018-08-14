5d2b7810d8865c039d1526295de39accae9dfeb6
Kiel Dehnert <kielzor@gmail.com>
2018-08-14 17:41:08 -0600

    Display cards with relevant information

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
 import React, { Component } from 'react';
 import './App.css';
+import CardContainer from './CardContainer';
+import DistrictRepository from './Helper'
+import kinderData from './data/kindergartners_in_full_day_program.js';
+
+const district = new DistrictRepository(kinderData)
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      data: []
+    }
+  }
+
+  componentDidMount = () => {
+    this.updateCards()
+  }
+
+  updateCards = (string) => {
+    let cardList = district.findAllMatches(string)
+    this.setState({
+      data: cardList
+    })
+  }
+
   render() {
     return (
-      <div>Welcome To Headcount 2.0</div>
+      <div>
+        <CardContainer data={this.state.data}/>
+      </div>
     );
   }
 }
--- src/helper.js
+++ src/helper.js
@@ -1,5 +1,3 @@
-import { stat } from "fs";
-
 export default class DistrictRepository {
   constructor(data) {
     this.stats = this.removeDuplicates(data)
@@ -8,12 +6,14 @@ export default class DistrictRepository {
   removeDuplicates = (data) => {
     return data.reduce((acc, stat) => {
       let name = stat.Location.toUpperCase();
+
       if (!acc[name]) {
         acc[name] = {
           location: name,
           stats: {}
         }
       }
+
       acc[name].stats[stat.TimeFrame] = Math.round(1000 * stat.Data) / 1000 || 0;
       return acc
     }, {})
@@ -27,12 +27,13 @@ export default class DistrictRepository {
 
   findAllMatches = (search) => {
     let arr = Object.keys(this.stats).map(key => this.stats[key]);
-      if (!search) {
-        return arr
-      }
+
+    if (!search) return arr
+    
     let matches = arr.filter(match => {
       return match.location.includes(search.toUpperCase())
     })
+
     return matches
   }
 }
