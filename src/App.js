5ab327010a031b276ab107d34513b7fcec188fbe
Kurt Miller <kmiller9393@gmail.com>
2018-08-14 15:15:58 -0600

    Get cards to render on page with each district location and corresponding data on each card

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import React, { Component } from 'react';
+import DistrictRepository from './helper';
+import CardContainer from './CardContainer';
+import kinderdata from './data/kindergartners_in_full_day_program';
 import './App.css';
+const districts = new DistrictRepository(kinderdata);
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      districts
+    };
+  }
   render() {
-    return <div>Welcome To Headcount 2.0</div>;
+    return (
+      <div className="App">
+        <CardContainer districts={this.state.districts} />
+      </div>
+    );
   }
 }
 
--- src/Card.js
+++ src/Card.js
@@ -1,1 +1,22 @@
 import React from 'react';
+import './App.css';
+import PropTypes from 'prop-types';
+
+const Card = ({ stats, location }) => {
+  return (
+    <div className="Card">
+      <h3>{location}</h3>
+      {Object.keys(stats).map(stat => {
+        return (
+          <article>
+            <ul>
+              {stat}: {stats[stat]}
+            </ul>
+          </article>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Card;
