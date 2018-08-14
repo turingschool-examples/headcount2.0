eeac3c64a8883bbc4355f442f39732dbea36ac04
Michael J. Lyons <mikelyons7@me.com>
2018-08-14 16:32:09 -0600

    Add flexbox to cards

--- src/DistrictsContainer.js
+++ src/DistrictsContainer.js
@@ -1,11 +1,12 @@
 import React from "react";
 import DistrictCard from "./DistrictCard";
+import "./DistrictsContainer.css";
 
 const DistrictsContainer = ({ districts }) => {
   const districtCard = Object.keys(districts.stats).map((district, index) => {
     return <DistrictCard districts={districts.stats[district]} key={index} />;
   });
-  return <div>{districtCard}</div>;
+  return <div className="district-container">{districtCard}</div>;
 };
 
 export default DistrictsContainer;
