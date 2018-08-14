79ee5bfa31a23d9992f1b3afd805149bc0939c3f
Chris Boylen <billpolster@gmail.com>
2018-08-14 17:29:09 -0600

    Add function to card to iterate over stats

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,7 +1,16 @@
 import React from 'react';
 
-const Card = () => {
+const Card = ({ location, stats}) => {
+  const displayStats = Object.entries(stats).map((stat, i) => {
+    return <div key={ i }>{stat[0]} : {stat[1]}</div>
+  })
   
+  return (
+    <article className="card">
+      <h1>{location}</h1>
+      <ul>{displayStats}</ul>
+    </article>
+  )
 }
 
 export default Card;
