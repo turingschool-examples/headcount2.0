a9fa8724c1a085dee6960d6378cde25dffa9a2a6
Gray Smith <gray.smith00@gmail.com>
2018-08-13 21:06:46 -0600

    Work on styling for App, DistrictContainer, and DistrictCard components

--- src/components/DistrictContainer.js
+++ src/components/DistrictContainer.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import DistrictCard from './DistrictCard';
+import '../css/DistrictContainer.css';
 
 const DistrictContainer = ({ category }) => {
   let districts;
