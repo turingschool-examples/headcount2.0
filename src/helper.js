cc24cb270f53f9dea8ad13baf22966bb990b657a
Brandon Fiebiger <brandonfiebiger@gmail.com>
2018-08-13 19:05:14 -0600

    First commit

--- src/helper.js
+++ src/helper.js
@@ -1,3 +1,29 @@
 export default class DistrictRepository {
+    constructor(stats) {
+        this.stats = this.removeDuplicates(stats)
+    }
+    removeDuplicates = (data) => {
+        return data.reduce((newObject, pieceOfData) => {
+            console.log(pieceOfData.TimeFrame)
+            let upperCasedData = pieceOfData.Location.toUpperCase();
+            if (!newObject[upperCasedData]) {
+                newObject[upperCasedData] = 
+                {location: upperCasedData,
+                 stats: {}
+                };
+       
+            }
+            newObject[upperCasedData].stats[pieceOfData.TimeFrame] = Math.round(pieceOfData.Data)
+            console.log(newObject['COLORADO'].stats)
+            return newObject
+        }, {})
+    }
 
+    findByName = (name) => {
+      if(!name) {
+       return
+      }
+      name = name.toUpperCase();
+      return this.stats[name];
+    }
 }
