34b3ba71efbf905a37684653513616d94c1626d2
Joel O'Connor <joconno07@gmail.com>
2018-08-13 22:56:40 -0600

    iteration part 1 tests passing

--- src/helper.js
+++ src/helper.js
@@ -3,18 +3,41 @@ export default class DistrictRepository {
     this.stats = this.districtDataCleaner(schoolData);
   }
 
-  districtDataCleaner(intitialData) {
-    return intitialData.reduce((organizedData, school) => {
-      if (!organizedData[school.Location]) {
-        organizedData[school.Location] = [school];
+  districtDataCleaner(initialData) {
+    return initialData.reduce((organizedData, school) => {
+      const sanitizedLocation = school.Location.toUpperCase();
+      const { TimeFrame, Data } = school;
+      let roundedData = 0;
+
+      if (isNaN(Data)) {
+        roundedData = 0;
       } else {
-        organizedData[school.Location] = [school, ...organizedData[school.Location]];
+        roundedData = Math.round(Data * 1000) / 1000;
+      }
+
+      if (!organizedData[school.Location]) {
+        organizedData[sanitizedLocation] = {
+          location: sanitizedLocation,
+          stats: {[TimeFrame]:roundedData}
+        };
+      } else {
+        organizedData[sanitizedLocation] = {
+          location: sanitizedLocation,
+          stats: {[TimeFrame]:roundedData, ...organizedData[sanitizedLocation].stats}
+        };
       }
       return organizedData;
     }, {});
   }
 
   findByName(userInput) {
-    return this.stats[userInput];
+    if (userInput) {
+      const sanitizedInput = userInput.toUpperCase();
+      return this.stats[sanitizedInput];
+    }
+  }
+
+  findAllMatches(userInput) {
+    
   }
 }
--- src/test/unit/iteration-1-part1.test.js
+++ src/test/unit/iteration-1-part1.test.js
@@ -12,7 +12,7 @@ describe('DistrictRepository iteration 1 - part 1', () =>  {
     expect(district.findByName('Vikings')).toBe(undefined);
   });
 
-  test.only('findByName returns an object with its individual district information', () => {
+  test('findByName returns an object with its individual district information', () => {
     expect(typeof district.findByName('Colorado')).toEqual('object');
     expect(district.findByName('Colorado').location).toEqual('COLORADO');
   });
