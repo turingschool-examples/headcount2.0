6414818c5ffcec3c5b4277f69627cfabff631a24
Laura Whitaker <laura.k.whitaker@gmail.com>
2018-08-14 15:26:40 -0600

    Add it blocks

--- src/test/App.test.js
+++ src/test/App.test.js
@@ -3,14 +3,27 @@ import ReactDOM from 'react-dom';
 import App from '../App';
 import { shallow } from 'enzyme';
 
+import DistrictRepository from '../helper.js';
+import kinderData from '../data/kindergartners_in_full_day_program.js';
+
+
 describe('App', () => {
-  it('should update state when componentDidMount', () => {
-    const wrapper = shallow(<App />)
-    const expected = {}
+  let wrapper;
+  let district;
+
+  beforeEach(() => {
+    wrapper = shallow(<App />)
+    district = new DistrictRepository(kinderData);
+  })
+
+  it('should update state with a card when selectLocation is called', () => {
+    const expected = [{ location: 'Colorado', stats: 0 }]
+    wrapper.instance().setState({ location: district.stats, cards: [] })
+
+    wrapper.instance().selectLocation('Colorado')
+    console.log(wrapper.instance().state.cards)
+    expect(wrapper.state('cards')).toEqual(expected)
   });
 
-  it('should update state when selectLocation is called', () => {
-
-  });
-
+  it('should not ')
 })
