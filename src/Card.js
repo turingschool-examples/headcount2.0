4bb6e993969b90a7c484d0e683d21a2a319115b5
John DoeMeganHaubelt <meganhaubelt@gmail.com>
2018-08-14 16:27:41 -0600

    Displaying all cards on DOM

--- src/Card.js
+++ src/Card.js
@@ -1,1 +1,17 @@
 import React from 'react';
+
+const Card = ({location, stats}) => {
+	const displayStats = stats.map( stat => <h4>{stat}</h4>)
+	return(
+		<div>
+			<h2>{location}</h2>
+			<p>{displayStats}</p>
+
+		</div>
+
+	) 
+
+
+}
+
+export default Card;
--- src/CardContainer.js
+++ src/CardContainer.js
@@ -1,5 +1,24 @@
 import React from 'react';
+import Card from './Card';
 
-const CardContainer = () => {
+const CardContainer = ({districtData}) => {
+	const districtKeys = Object.keys(districtData.stats)
 	
+	const displayCards = districtKeys.map(district => (
+		// let location = districtData.stats[district].location
+		<Card
+			location = {districtData.stats[district].location}
+			stats = {Object.entries(districtData.stats[district].stats)}
+		/>
+	))
+
+	
+
+	return(
+		<div>
+			{displayCards}
+		</div>
+	)
 }
+
+export default CardContainer;
