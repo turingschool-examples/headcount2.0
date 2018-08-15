13be2984db995a4bbedd5dfb1400a2936c12abb9
benjaminpaige <benjamin.paige@gmail.com>
2018-08-15 09:22:57 -0600

    Add constructor and state object to app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
+import DistrictRepository from './helper.js';
+import kinderData from './data/kindergartners_in_full_day_program.js';
 import React, { Component } from 'react';
 import './App.css';
+import DistrictContainer from './DistrictContainer.js';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+     data: {} 
+    }
+  }
+
+  componentDidMount = () => {
+    const district = new DistrictRepository(kinderData);
+    this.setState({ data: district.stats});
+  }
+
   render() {
     return (
-      <div>Welcome To Headcount 2.0</div>
+      <div>
+
+        {/* inputs */}
+
+        {/* district container */}
+        <DistrictContainer data={this.state.data}/>
+
+      </div>
     );
   }
 }
