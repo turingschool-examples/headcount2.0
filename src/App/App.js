83e79566b703904a74a092632ca09561b98581d7
Joel O'Connor <joconno07@gmail.com>
2018-08-15 10:57:07 -0600

    Added in app heading and class names

--- src/App/App.js
+++ src/App/App.js
@@ -10,13 +10,29 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      data: new DistrictRepository(kinderData)
+      data: new DistrictRepository(kinderData).findAllMatches(`${this.state.userInput}`),
+      userInput: ''
     };
+  }
+
+  handleSearch = (e) => {
+    const {value, name} = e.target;
+    this.setState({
+      [name]:value
+    });
   }
 
   render() {
     return (
       <div>
+        <h1 className='main-heading'>HeadCount 2.0</h1>
+        <input 
+          onChange={this.handleSearch}
+          className='search-bar' 
+          value={this.state.search}
+          name='userInput'
+          placeholder='Search'
+        />
         <CardContainer schoolData={this.state.data} />
       </div>
     );
