8002eeb02036439c89fe69abc372ecd18ac6c419
benjaminpaige <benjamin.paige@gmail.com>
2018-08-15 14:18:18 -0600

    Add stat results to data table in card component

--- src/Card.js
+++ src/Card.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import './Card.css';
+
 
 export default class Card extends Component {
 
@@ -6,12 +8,18 @@ export default class Card extends Component {
     return (
       <aside key={this.props.key}> 
         <h3>{this.props.district.location}</h3>
-        <ul>
+        <table>
           {Object.keys(this.props.district.stats).map((stat, index) => {
-            return <li key={index}>{stat} {this.props.district.stats[stat]} </li>;
+            return (
+              <tr className={`${this.props.district.stats[stat] > .5 ? 'aboveHalf' : ''}`} key={index}>
+                <td>{stat}</td>
+                <td  >{this.props.district.stats[stat]}</td>
+              </tr>
+            );
           })}
-        </ul>
+        </table>
       </aside>
     );
+    
   }
 }
