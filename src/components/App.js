a922f100569f509f29c43ad3de0cee4ae10ef040
Gray Smith <gray.smith00@gmail.com>
2018-08-15 15:43:10 -0600

    Change App state to include an array of districts

--- src/components/App.js
+++ src/components/App.js
@@ -4,12 +4,14 @@ import '../css/App.css';
 import kinderGartenData from '../data/kindergartners_in_full_day_program';
 import DistrictRepository from '../helper';
 import DistrictContainer from './DistrictContainer';
+import Search from './Search';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
       category: {},
+      districts: [],
       loading: true
     };
   }
@@ -20,9 +22,19 @@ class App extends Component {
 
   populateDistrictData = data => {
     const category = new DistrictRepository(data);
+    const districts = category.findAllMatches();
     this.setState({
       category,
+      districts,
       loading: false
+    });
+  };
+
+  filterCards = string => {
+    const category = new DistrictRepository(kinderGartenData);
+    const districts = category.findAllMatches(string);
+    this.setState({
+      districts
     });
   };
 
@@ -31,7 +43,11 @@ class App extends Component {
       <div className="app">
         <div className="container">
           <h1>Welcome To Headcount 2.0</h1>
-          <DistrictContainer category={this.state.category} />
+          <Search filterCards={this.filterCards} />
+          <DistrictContainer
+            districts={this.state.districts}
+            loading={this.state.loading}
+          />
         </div>
       </div>
     );
