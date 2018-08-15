13819b430dee3a0bf4ea2da7d37b1c84d108f962
Jesse McFadden <jessemcbrennan@gmail.com>
2018-08-15 10:58:45 -0600

    Refactor to include search functionality

--- src/App.js
+++ src/App.js
@@ -3,28 +3,36 @@ import DistrictRepository from './helper'
 import './App.css';
 import kinderData from './data/kindergartners_in_full_day_program.js';
 import CardContainer from './CardContainer'
+import Search from './Search'
 
-// const allDistricts = new DistrictRepository(kinderData)
+const allDistricts = new DistrictRepository(kinderData)
 
 class App extends Component {
   constructor(props) {
     super(props)
-    this.allDistricts = new DistrictRepository(kinderData)
+    // this.allDistricts = new DistrictRepository(kinderData)
     this.state = {
-      data: this.allDistricts.stats
+          data: []
     }
-
   }
 
-  // componentDidMount() {
-  //   const district = Object.keys(this.allDistricts.stats)
-  //   this.setState({districtData: district})
-  // }
+  componentDidMount() {
+    this.updateCards();
+  }
+
+  updateCards = (string) => {
+    let cardData = allDistricts.findAllMatches(string)
+
+    this.setState({
+      data: cardData
+    }) 
+  }
 
   render() {
     return (
       <div className="headerContainer">
         <h1>HeadCount 2.0</h1>
+        <Search updateCards={this.updateCards}/>
         <CardContainer data={ this.state.data }/>
       </div>
     );
