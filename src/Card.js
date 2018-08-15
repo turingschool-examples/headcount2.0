29679d209deb0ceae905280bff2756b8021e78e2
Jesse McFadden <jessemcbrennan@gmail.com>
2018-08-15 09:54:20 -0600

    Add ternary to Card.js file to allow for conditional rendering of stats less than .5

--- src/Card.js
+++ src/Card.js
@@ -2,8 +2,8 @@ import React from 'react'
 import './Card.css'
 
 const Card = ({ location, stats }) => {
+  const displayStats = Object.entries(stats).map(stat => <li className={`greaterThan ${ stat[1] < .5 ? 'lessThan' : ''}`}>{stat[0]}: {stat[1]}</li>)
 
-  const displayStats = Object.entries(stats).map(stat => <li>{stat[0]}: {stat[1]}</li>)
   return(
   <div className="card">  
     <h4>{location}</h4>
