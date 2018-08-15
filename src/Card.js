49046bbe47e37cc98b7e21c668f7d11226bf72db
benjaminpaige <benjamin.paige@gmail.com>
2018-08-15 09:23:56 -0600

    Add Card component

---
+++ src/Card.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+export default class Card extends Component {
+
+  render() {
+    return (
+      <aside> 
+       <h3>
+
+       </h3>
+      </aside>
+    )
+  }
+}
