cb85f5aaea2da1225f51a7bc26bfcc52f9da1ab9
benjaminpaige <benjamin.paige@gmail.com>
2018-08-15 17:46:13 -0600

    add componentDidMount and searchDistricts methods to app component

--- src/App.js
+++ src/App.js
@@ -3,30 +3,37 @@ import kinderData from './data/kindergartners_in_full_day_program.js';
 import React, { Component } from 'react';
 import './App.css';
 import DistrictContainer from './DistrictContainer.js';
+import Search from './Search';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-     data: {} 
+      district: {},
+      data: {},
+      searchResults: []
     }
   }
 
   componentDidMount = () => {
     const district = new DistrictRepository(kinderData);
-    this.setState({ data: district.stats});
+    this.setState({ data: district.stats, district});
   }
 
+  searchDistricts = (input) => {
+    const searchResults = this.state.district.findAllMatches(input);
+    this.setState({ searchResults });
+  }
+
+  componentDidUpdate() {
+
+  }
   render() {
     return (
-      <div>
-
-        {/* inputs */}
-
-        {/* district container */}
+      <main>
+        <Search searchDistricts={this.searchDistricts}/>
         <DistrictContainer data={this.state.data}/>
-
-      </div>
+      </main>
     );
   }
 }
