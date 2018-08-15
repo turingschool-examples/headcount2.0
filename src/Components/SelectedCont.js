744f4d07bf6c76885c3665713df2ed7b03d67685
Chris Boylen <billpolster@gmail.com>
2018-08-15 11:47:59 -0600

    Add selectedcard component

---
+++ src/Components/SelectedCont.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Card from './Card';
+import PropTypes from 'prop-types';
+import helper from '../helper';
+
+const SelectedCont = ({ data, selectedCont }) => {
+  
+}
+
+export default SelectedCont;
