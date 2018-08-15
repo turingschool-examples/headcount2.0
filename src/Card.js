47d23861c6db9dd750009696c643be4ec4f16650
benjaminpaige <benjamin.paige@gmail.com>
2018-08-15 09:44:52 -0600

    Create Card component return and append district information

--- src/Card.js
+++ src/Card.js
@@ -4,11 +4,14 @@ export default class Card extends Component {
 
   render() {
     return (
-      <aside> 
-       <h3>
-
-       </h3>
+      <aside key={this.props.key}> 
+        <h3>{this.props.district.location}</h3>
+        <ul>
+          {Object.keys(this.props.district.stats).map((stat, index) => {
+            return <li key={index}>{stat} {this.props.district.stats[stat]} </li>;
+          })}
+        </ul>
       </aside>
-    )
+    );
   }
 }
