8cfd9a6ba01bdac89fe0bdc5e57884daccb1c656
benjaminpaige <benjamin.paige@gmail.com>
2018-08-15 22:38:09 -0600

    Add selectDistrict method to App.js

--- src/App.js
+++ src/App.js
@@ -10,26 +10,40 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      districts: []
+      districts: [],
+      selectedDistricts: []
     };
   }
 
   componentDidMount = () => {
     this.searchDistricts(undefined);
-    console.log(this.state.districts);
   }
 
-  searchDistricts = (input) => {
+  searchDistricts = input => {
     let districts = district.findAllMatches(input);
     districts.forEach(district => district.key = district.location);
     this.setState({ districts });
+  }
+
+  selectDistrict = key => {
+    let selectedDistricts = district.findByName(key);
+    if (this.state.selectedDistricts.length > 1) {
+      this.setState({selectedDistricts : this.state.selectedDistricts.shift()})
+    }
+    this.setState({ selectedDistricts : [...this.state.selectedDistricts, selectedDistricts] });
   }
 
   render() {
     return (
       <main>
         <Search searchDistricts={this.searchDistricts}/>
-        <DistrictContainer districts={this.state.districts}/>
+        <DistrictContainer 
+          districts={this.state.selectedDistricts} 
+        />
+        <DistrictContainer 
+          districts={this.state.districts}
+          selectDistrict={this.selectDistrict}
+        />
       </main>
     );
   }
