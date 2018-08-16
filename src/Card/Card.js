f4b85d8a3c3d05bdbcf468c406447ace036c5bbc
Joel O'Connor <joconno07@gmail.com>
2018-08-15 18:54:57 -0600

    Added prop tests to Card

--- src/Card/Card.js
+++ src/Card/Card.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
 import './Card.css';
 
 export const Card = ({location, stats}) => {
-  const displayStats = Object.entries(stats).map(data => (
-    <li key={Date.now() * Math.random()} className={`${data[1] < .5 ? 'red' : 'green' }`}>{data[0]}: {data[1]}</li>
+  const displayStats = Object.entries(stats).map((data, i) => (
+    <li 
+      key={i} 
+      className={`${data[1] < .5 ? 'red' : 'green'}`}
+    >{data[0]}: {data[1]}</li>
   ));
 
   return (
@@ -12,4 +17,9 @@ export const Card = ({location, stats}) => {
       <ul className='stats-list'>{displayStats}</ul>
     </div>
   );
+};
+
+Card.propTypes = {
+  location: PropTypes.string,
+  stats: PropTypes.object
 };
