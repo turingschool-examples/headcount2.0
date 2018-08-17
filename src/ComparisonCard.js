54153b7dd4911ab4c3a161e30e44ff9cfd18f486
benjaminpaige <benjamin.paige@gmail.com>
2018-08-16 20:49:07 -0600

    Change formatting for ComparisonCard

--- src/ComparisonCard.js
+++ src/ComparisonCard.js
@@ -4,15 +4,15 @@ export const ComparisonCard = (props) => {
   const keys = Object.keys(props.comparisonData)
   return (
     <div>
-      <h3>
-      {keys[0]} AVG: {props.comparisonData[keys[0]]} 
-      </h3>
-      <h3>
-      {keys[1]} AVG: {props.comparisonData[keys[1]]} 
-      </h3>
-      <h4>
-        {props.comparisonData.compared}
-      </h4>
+      <div>
+        <h4>{keys[0]}</h4>
+        <h4>{keys[1]}</h4>
+      </div>
+      <div>
+        <h4>{props.comparisonData[keys[0]]}</h4>
+        <h4>{props.comparisonData[keys[1]]}</h4>
+      </div>
+      <h3>{props.comparisonData.compared}</h3>
     </div>
   );
 };
