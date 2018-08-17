af28cbc8a6140184b3bf886a70814b8563222c27
benjaminpaige <benjamin.paige@gmail.com>
2018-08-16 18:10:48 -0600

    Add functionality to create comparison data in state of app

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import React, { Component } from 'react';
 import './App.css';
 import DistrictContainer from './DistrictContainer.js';
 import Search from './Search';
+import { ComparisonCard } from './ComparisonCard';
 
 const district = new DistrictRepository(kinderData);
 class App extends Component {
@@ -11,7 +12,9 @@ class App extends Component {
     super();
     this.state = {
       districts: [],
-      selectedDistricts: []
+      selectedDistricts: [],
+      comparisonData: {},
+      showComparison: false
     };
   }
 
@@ -25,13 +28,33 @@ class App extends Component {
     this.setState({ districts });
   }
 
+  componentDidUpdate = () => {
+    // console.log(this.state.selectedDistricts);
+    // console.log(this.state.selectedDistricts.length);
+    // console.log(this.comparisonData);
+  }
+
   selectDistrict = key => {
     let selectedDistricts = district.findByName(key);
     if (this.state.selectedDistricts.length > 1) {
-      this.setState({selectedDistricts : this.state.selectedDistricts.shift()})
+      this.setState({showComparison: true})
+      this.setState({selectedDistricts : this.state.selectedDistricts.shift()});
+      const d1 = this.state.selectedDistricts[0].location;
+      const d2 = key;
+      let comparisonData = district.compareDistrictAverages(d1, d2);
+      this.setState({ comparisonData});
+
     }
-    this.setState({ selectedDistricts : [...this.state.selectedDistricts, selectedDistricts] });
+    this.setState({ selectedDistricts : [...this.state.selectedDistricts, selectedDistricts] });    
   }
+
+  //add pointer cursor to cards and hover effect
+  //add header to app component render
+  //add comparative data card for top cards component
+  //refactor
+  //prop-types for all components
+  //test all components
+
 
   render() {
     return (
@@ -40,6 +63,7 @@ class App extends Component {
         <DistrictContainer 
           districts={this.state.selectedDistricts} 
         />
+    {this.state.showComparison && <ComparisonCard comparisonData={this.state.comparisonData}/>}
         <DistrictContainer 
           districts={this.state.districts}
           selectDistrict={this.selectDistrict}
