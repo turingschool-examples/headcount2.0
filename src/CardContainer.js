f89d1c06d7219ca3d2a48587e35c422d8576a297
Laura Whitaker <laura.k.whitaker@gmail.com>
2018-08-17 08:28:40 -0600

    Fix eslint CardContainer

--- src/CardContainer.js
+++ src/CardContainer.js
@@ -2,25 +2,29 @@ import React from 'react';
 import Card from './Card';
 
 const CardContainer = ({ cards, averages }) => {
-    const displayCards = cards.map((card, i) => (
-        < Card {...card} key={i} />
-    ));
+  const displayCards = cards.map((card, index) => (
+    <Card {...card} key={index} />
+  ));
 
-    let displayCompared = <p></p>
+  let displayCompared = <p></p>;
 
-    if (averages.compared) {
-        displayCompared = <p className='compared-average'>COMPARED AVERAGES: {averages.compared}</p>
-    }
+  if (averages.compared) {
+    displayCompared = <p className='compared-average'>
+                        COMPARED AVERAGES: {averages.compared}
+                      </p>;
+  }
 
-    return (
-        <div className='CardContainer'>
-            <h1 className='CardContainer__header'>KINDERGARTNERS IN FULL DAY PROGRAM</h1>
-            <section className='CardContainer__section'>
-                {displayCards}
-            </section>
-            {displayCompared}
-        </div>
-    )
-}
+  return (
+    <div className='CardContainer'>
+      <h1 className='CardContainer__header'>
+            KINDERGARTNERS IN FULL DAY PROGRAM
+      </h1>
+      <section className='CardContainer__section'>
+        {displayCards}
+      </section>
+      {displayCompared}
+    </div>
+  );
+};
 
 export default CardContainer;
