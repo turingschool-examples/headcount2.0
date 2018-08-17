b2962549a40c8cb50fd44370f94480f221d1780a
Cody Taft <cody.taft@gmail.com>
2018-08-16 20:12:48 -0600

    Finish cardcontainer tests

---
+++ src/CardContainer.test.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { CardContainer } from './CardContainer.js'
+
+describe('CardContainer', () => {
+
+  it('should match the snapshot with no cards', () => {
+    const mockProp = {
+                      districts: []
+    }
+    const wrapper = shallow(<CardContainer districts={mockProp}/>)
+    expect(wrapper).toMatchSnapshot()
+  })
+
+  it('should match the snapshot with cards', () => {
+    const mockProp = {
+                      districts: [{
+                        location: "COLORADO",
+                        stats: {2004: 0.24}
+                      }]
+    }
+    const wrapper = shallow(<CardContainer districts={mockProp}/>)
+    expect(wrapper).toMatchSnapshot()
+  })
+
+
+})
--- src/DistrictCard.test.js
+++ src/DistrictCard.test.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-
 import { DistrictCard } from './DistrictCard.js'
 
 describe('DistrictCard', () => {
