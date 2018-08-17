163894a0022d4bdf8d38663abb37e34b90026f6c
benjaminpaige <benjamin.paige@gmail.com>
2018-08-16 21:31:29 -0600

    Add classNames to ComparisonCard component for styling

--- src/ComparisonCard.js
+++ src/ComparisonCard.js
@@ -1,18 +1,19 @@
 import React from 'react';
+import './ComparisonCard.css';
 
 export const ComparisonCard = (props) => {
   const keys = Object.keys(props.comparisonData)
   return (
-    <div>
-      <div>
+    <div className="comparison-card">
+      <div className="district-info">
         <h4>{keys[0]}</h4>
         <h4>{keys[1]}</h4>
       </div>
-      <div>
+      <div className="district-info">
         <h4>{props.comparisonData[keys[0]]}</h4>
         <h4>{props.comparisonData[keys[1]]}</h4>
       </div>
-      <h3>{props.comparisonData.compared}</h3>
+      <h3 className="comparison-ratio">{props.comparisonData.compared}</h3>
     </div>
   );
 };
