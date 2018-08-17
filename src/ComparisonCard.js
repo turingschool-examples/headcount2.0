0528bc9310bb7b8fcfd82b62e7ed5b93f91045b1
benjaminpaige <benjamin.paige@gmail.com>
2018-08-16 19:49:55 -0600

    Add comparison card data to component

--- src/ComparisonCard.js
+++ src/ComparisonCard.js
@@ -1,9 +1,18 @@
 import React from 'react';
 
-export const ComparisonCard = ({comparisonData}) => {
-
-  console.log(comparisonData)
+export const ComparisonCard = (props) => {
+  const keys = Object.keys(props.comparisonData)
   return (
-    <h1>Hello</h1>
+    <div>
+      <h3>
+      {keys[0]} AVG: {props.comparisonData[keys[0]]} 
+      </h3>
+      <h3>
+      {keys[1]} AVG: {props.comparisonData[keys[1]]} 
+      </h3>
+      <h4>
+        {props.comparisonData.compared}
+      </h4>
+    </div>
   );
 };
