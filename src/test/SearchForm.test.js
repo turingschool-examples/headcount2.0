5e0e0f3b06d295d99fb302b5a61d280a7a61f23e
Michael J. Lyons <mikelyons7@me.com>
2018-08-17 09:01:03 -0600

    Update searchForm test

--- src/test/SearchForm.test.js
+++ src/test/SearchForm.test.js
@@ -3,23 +3,23 @@ import SearchForm from "../components/SearchForm";
 import { shallow } from "enzyme";
 
 describe("SearchForm", () => {
-  it("it updates the state when the input value changes", () => {
-    const wrapper = shallow(<SearchForm findDistricts={jest.fn()} />);
+  const mockFn = jest.fn();
+  let shallowWrapper = shallow(<SearchForm findDistricts={mockFn} />);
 
+  it("it updates the state when the input value changes", () => {
     const mockEvent = { target: { value: "mike" } };
     const anticipatedState = "mike";
 
-    wrapper.instance().handleChange(mockEvent);
-    expect(wrapper.state().districtSeach).toEqual(anticipatedState);
+    shallowWrapper.instance().handleChange(mockEvent);
+    expect(shallowWrapper.state().districtSearch).toEqual(anticipatedState);
   });
 
   it("should update state when handleInput is invoked", () => {
-    const wrapper = shallow(<SearchForm findDistricts={jest.fn()} />);
     const mockInput = { target: { value: "mike" } };
     const anticipatedState = "mike";
 
-    wrapper.instance().handleChange(mockInput);
+    shallowWrapper.instance().handleChange(mockInput);
 
-    expect(wrapper.state("districtSearch")).toEqual(anticipatedState);
+    expect(shallowWrapper.state("districtSearch")).toEqual(anticipatedState);
   });
 });
