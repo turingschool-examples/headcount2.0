3c2116145878f83a427b379c428e3caf8474c1ce
Laura Whitaker <laura.k.whitaker@gmail.com>
2018-08-16 19:06:25 -0600

    Add LocationList tests

--- src/test/LocationList.test.js
+++ src/test/LocationList.test.js
@@ -3,23 +3,26 @@ import LocationList from '../LocationList';
 import { shallow } from 'enzyme';
 
 describe('LocationList', () => {
-    let wrapper;
-    let selectLocationMock;
-
-    beforeEach(() => {
-        selectLocationMock = jest.fn()
-        wrapper = shallow(<LocationList locations={['Colorado']} selectLocation={selectLocationMock} />)
-    })
 
     it('should match the snapshot', () => {
+        const wrapper = shallow(<LocationList displayedLocations={[]} />)
         expect(wrapper.html()).toMatchSnapshot()
     })
 
-    it('should create locationButtons', () => {
-
+    it('should match the snapshot with one location', () => {
+        const wrapper = shallow(<LocationList displayedLocations={['COLORADO']} />)
+        expect(wrapper.html()).toMatchSnapshot()
     })
 
-    it('should call selectLocation when location button is clicked', () => {
+    it('should match the snapshot with multiple locations', () => {
+        const wrapper = shallow(<LocationList displayedLocations={['COLORADO', 'ACADEMY 20', 'AGATE 300']} />)
+        expect(wrapper.html()).toMatchSnapshot()
+    })
+
+    it('should call selectLocation when a location button is clicked', () => {
+        const selectLocationMock = jest.fn()
+        const wrapper = shallow(<LocationList displayedLocations={['COLORADO']} selectLocation={selectLocationMock} />)
+
         wrapper.find('.LocationList__btn').simulate('click')
 
         expect(selectLocationMock).toHaveBeenCalled()
