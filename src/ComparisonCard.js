f6703c9015ba8a826f1bfacbeee57806b507735b
benjaminpaige <benjamin.paige@gmail.com>
2018-08-17 22:51:54 -0600

    Remove props from Card render after destructuring props in parameters

--- src/ComparisonCard.js
+++ src/ComparisonCard.js
@@ -10,10 +10,10 @@ export const ComparisonCard = ({ comparisonData }) => {
         <h4>{keys[1]}</h4>
       </div>
       <div className="district-info">
-        <h4>{props.comparisonData[keys[0]]}</h4>
-        <h4>{props.comparisonData[keys[1]]}</h4>
+        <h4>{comparisonData[keys[0]]}</h4>
+        <h4>{comparisonData[keys[1]]}</h4>
       </div>
-      <h3 className="comparison-ratio">{props.comparisonData.compared}</h3>
+      <h3 className="comparison-ratio">{comparisonData.compared}</h3>
     </div>
   );
 };
