d64bdb67e5c4d467f425c3b6b3b71589c4a02d81
Jesse McFadden <jessemcbrennan@gmail.com>
2018-08-18 21:21:28 -0600

    Add selected class to Card component

--- src/Card.js
+++ src/Card.js
@@ -2,11 +2,11 @@ import React from 'react'
 import './Card.css'
 import PropType from 'prop-types'
 
-const Card = ({ location, stats, selectCards }) => {
+const Card = ({ location, stats, selectCards, selected }) => {
   const displayStats = Object.entries(stats).map((stat,index) => <li key={index} className={`greaterThan ${ stat[1] < .5 ? 'lessThan' : ''}`}>{stat[0]}: {stat[1]}</li>)
 
   return(
-  <div className="card" onClick={() => selectCards(location)}>  
+  <div className={`Card ${selected ? 'selected' :  'card'}`} onClick={() => selectCards(location)}>  
     <h1 className="cardHeader">{location}</h1>
     <ul>{displayStats}</ul>
   </div>
