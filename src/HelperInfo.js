0abb45551a175c6890aeb16ef54c925fbf8fcecd
Laura Whitaker <laura.k.whitaker@gmail.com>
2018-08-19 12:37:18 -0600

    Update helper info text

--- src/HelperInfo.js
+++ src/HelperInfo.js
@@ -8,7 +8,8 @@ const HelperInfo = ({ toggleHelperInfo }) => {
         Compare district stats by clicking on two districts from the list 
         on the left. To change districts click on the districts in the list 
         on the left or remove them individually by clicking on the district 
-        card itself.
+        card itself. Search for districts in upper left corner. To change 
+        data set click on title and select from dropdown.
       </p>
       <button onClick={ () => toggleHelperInfo() }>X</button>
     </div>
