c7615cffd494ca64d92bb2db0d2b733db63e91cd
Jesse McFadden <jessemcbrennan@gmail.com>
2018-08-19 14:25:02 -0600

    Add working unselectCard functionality and method

--- src/App.js
+++ src/App.js
@@ -28,42 +28,33 @@ class App extends Component {
     }) 
   }
 
-  unselectCards = (location) => {
-    const selectedCard = this.statetoSelectedCards
-    const filteredCards = this.state.selectedCards.filter(card => card.location !== selectedCard.location)
-
-    this.setSTate({ selectedCards: filteredCards })
-  }
 
   selectCards = (location) => {
     this.compareCards(this.state.selectedCards)
     const selectedCard = allDistricts.findByName(location)
     const selectState = this.state.selectedCards
-    if(!selectState.includes(selectedCard) && selectState.length < 2) {
+
+    if (selectState.includes(selectedCard)) {
+      this.unselectCards(selectedCard.location)
+      return 
+    }
+
+    if(selectState.length < 2) {
       const selectedCards = [...this.state.selectedCards, selectedCard]
       this.setState({ selectedCards }), () => this.compareCards()
       selectedCard.isSelected = !selectedCard.isSelected
-      // const comparedCards = this.compareCards(selectedCard)
-      console.log(this.state.selectedCards)
     }
-    // if (selectState.includes(selectedCard)) {
-    //   this.
-    // }
+
   }
 
-//   selectCards = (location) => {
-//     const selectedCard = allDistricts.findByName(location)
-//     const selectedCards = [...this.state.selectedCards, selectedCard]
+  unselectCards = (location) => {
+    const selectedCard = this.state.selectedCards
+    const filteredCards = this.state.selectedCards.filter(card => card.location !== location)
 
-//     if(this.state.selectedCards.length < 2 && !this.state.selectedCards.includes(selectedCard)) {
-//       selectedCard.isSelected = !selectedCard.isSelected
-//       } 
-//     }
-//     this.setState({ selectedCards })
 
-//       this.compareCards(this.state.selectedCards)
-//   }
-// }
+    this.setState({ selectedCards: filteredCards })
+
+  }
 
   compareCards = (cards) => {
       if(this.state.selectedCards.length === 2) {
@@ -71,21 +62,22 @@ class App extends Component {
         const district2 = this.state.selectedCards[1].location
         
         var compareObject = allDistricts.compareDistrictAverages(district1, district2)
-        // console.log(compareObject)
+     
         this.setState({ compareObject })
     }
-
-
   }
 
   render() {
     return (
       <div className="headerContainer">
-        <h1>HeadCount 2.0</h1>
+        <h1 className="headerTitle">HeadCount 2.0</h1>
+        <div className="searchContainer">
         <Search 
           updateCards={this.updateCards}
         />
+        </div>
         <CardContainer
+          className="bottomContainer"
           data={this.state.selectedCards}
           selectCards={this.selectCards}
           
