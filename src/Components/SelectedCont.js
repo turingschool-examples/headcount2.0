846ac2e8df082fed561366b93f0c58b08f0019bf
Chris Boylen <billpolster@gmail.com>
2018-08-19 14:17:22 -0600

    Add conditional logic to display compareddata in selectedcont component

--- src/Components/SelectedCont.js
+++ src/Components/SelectedCont.js
@@ -1,10 +1,48 @@
 import React from 'react';
 import Card from './Card';
 import PropTypes from 'prop-types';
-import helper from '../helper';
 
-const SelectedCont = ({ data, selectedCont }) => {
-  
+const SelectedCont = ({ selectedCards, selectCard, compareDistrictAverages }) => {
+  const displaySelectedCards = Object.values(selectedCards).map((district, i) => {
+    return  <Card location={district.location}
+                  stats={district.stats}
+                  isSelected={district.isSelected}
+                  key={`sel-card${i}`}
+                  selectCard={selectCard}
+            />
+  })
+
+  const compareSelectedCards = () => {
+    if (selectedCards.length === 2) {
+      const location1 = selectedCards[0].location;
+      const location2 = selectedCards[1].location;
+      const comparedData = compareDistrictAverages(location1, location2); 
+    
+    return (
+      <div className="compare-cont">
+        {displaySelectedCards[0]}
+        <div className="compare-info">
+          <h2>{location1}{comparedData[location1]}</h2>
+          <h2>{comparedData.compared}</h2>
+          <h2>{location2}{comparedData[location2]}</h2>
+        </div>
+        {displaySelectedCards[1]}
+      </div>
+    )
+    } else {
+      return displaySelectedCards
+    }
+  }
+
+  return (
+    <article className="card-cont">
+      {compareSelectedCards()}
+    </article>
+  )
+}
+
+SelectedCont.propTypes = {
+  data: PropTypes.array
 }
 
 export default SelectedCont;
