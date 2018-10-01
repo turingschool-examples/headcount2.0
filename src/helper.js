a1efc7d27b7b77f886561f3a7d2490a9de9b7fd4
Drake Bott <bott.da@gmail.com>
2018-10-01 14:59:50 -0600

    Create DistrictRepository obejct, pass iteration 0 and iteration 1 part 1.

--- src/helper.js
+++ src/helper.js
@@ -1,3 +1,41 @@
 export default class DistrictRepository {
+  constructor(stats) {
+    this.stats = this.cleanData(stats);
+  }
 
+  findByName(search) {
+    if (!search) return undefined;
+    const searchResult = Object.keys(this.stats).find(keyName => {
+      return keyName.toLowerCase().includes(search.toLowerCase());
+    });
+
+    if (!searchResult) {
+      return undefined;
+    }
+
+    return {
+      location: searchResult,
+      stats: this.stats[searchResult]
+    };
+  }
+
+  findAllMatching(search) {
+  }
+
+  cleanData(stats) {
+    return stats.reduce((acc, data) => {
+      if (typeof data.Data != 'number') data.Data = 0;
+      const roundedData = Math.round(data.Data * 1000) / 1000;
+
+      if(acc[data.Location]) {
+        acc[data.Location][data.TimeFrame] = roundedData;
+      }
+      else {
+        acc[data.Location.toUpperCase()] = {
+          [data.TimeFrame]: roundedData
+        }
+      }
+      return acc;
+    }, {});
+  }
 }
