d80a6f93112e96674b2fbb7ae39d872b4368b04b
Cole Hart <cole@colehart.com>
2018-10-01 17:38:01 -0600

    Add setupTests.js and begin structure for helper.js

--- src/helper.js
+++ src/helper.js
@@ -1,3 +1,15 @@
 export default class DistrictRepository {
+  constructor(newData) {
+    this.stats = this.cleanUp(newData)
+  }
 
+  cleanUp(newData) {
+    return newData.reduce((cleanData, entry) => {
+      if (!cleanData.includes(entry)) {
+        cleanData.push(entry)
+      }
+
+      return cleanData
+    }, [])
+  }
 }
--- src/test/unit/iteration-0.test.js
+++ src/test/unit/iteration-0.test.js
@@ -11,8 +11,8 @@ describe('DistrictRepository iteration 0', () =>  {
 
   test('data coming in has no duplicates', () => {
     // uncomment out the tests that best fits your model
-    // expect(district.stats.length).toBe(181);
-    expect(Object.keys(district.stats).length).toBe(181);
+    expect(district.stats.length).toBe(181);
+    // expect(Object.keys(district.stats).length).toBe(181);
   });
 
 });
