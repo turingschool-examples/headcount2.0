36cde05b86a089436a50846b0e5b13c3f481ee44
Aaron Weissman <weissman.aaron@gmail.com>
2018-10-01 15:47:32 -0600

    first commit with iteration 0 and findByName from iteration 1

--- src/helper.js
+++ src/helper.js
@@ -1,3 +1,46 @@
 export default class DistrictRepository {
+  constructor(stats) {
+    this.stats = this.cleanData(stats)
+  }
+
+  cleanData = (data) => {
+    let cleanData = data.reduce( (schools, year) => {
+
+      let roundedYear = Math.round(year.Data * 1000)/1000 || 0;
+
+      if (!schools[year.Location]) {
+        schools[year.Location] = {
+          stats: {[year.TimeFrame]: roundedYear},
+          location: year.Location.toUpperCase()
+        }
+      } else {
+        Object.assign(schools[year.Location].stats, {[year.TimeFrame]: roundedYear});
+      }
+      return schools;
+    }, {});
+    return cleanData;
+  }
+
+  findByName = (enteredName) => {
+    const locations = Object.keys(this.stats)
+
+    if (enteredName) {
+      enteredName = enteredName.toLowerCase()
+      let correctDistrict = locations.find( location => {
+        if (enteredName === location.toLowerCase()) {
+          return location;
+        }
+      })
+      return this.stats[correctDistrict]
+    } else {
+      return undefined
+    }
+  }
+
+  findAllMatches = (entry) => {
+    const locations = Object.keys(this.stats)
+
+
+  }
 
 }
