b037a96e1fce36ffcd5d9673f8818583178c0067
Justin Stewart <Justinstewart3313@gmail.com>
2018-10-01 14:34:31 -0600

    Start to parse data

--- src/helper.js
+++ src/helper.js
@@ -1,3 +1,19 @@
-export default class DistrictRepository {
+import Kinderdata from './data/kindergartners_in_full_day_program.js'
 
+
+export default class DistrictRepository {
+  constructor(kinderData) {
+    this.stats = {};
+  }
+
+
+  makeStats = () => {
+    this.kinderData.reduce((unit, thingy) => {
+      if (!acc[thingy.Location]) {
+        acc[thingy.Location]
+      }
+
+      return acc[thingy.location[thingy.TimeFrame]] = [thingy.TimeFrame] 
+    }, this.stats)
+  }
 }
