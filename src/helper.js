057e24d222e43c2ce867c96b1aba410f0b78608b
Alexander Rau <alexanderrau86@yahoo.com>
2018-10-01 17:27:17 -0600

    Pass the first two tests looking for undefined result

--- src/helper.js
+++ src/helper.js
@@ -11,12 +11,25 @@ export default class DistrictRepository {
       }
       
       if(acc[item.Location]) {
-        acc[item.Location].push(item)
+        acc[item.Location].push({[item.TimeFrame]: item.Data})
       }
 
       return acc
     },{})
-    
+
     return stats
+  }
+
+  findByName(name) {
+    let statKeys = Object.keys(this.stats)
+    statKeys = statKeys.map((key) => {
+      return key.toUpperCase()
+    })
+
+    if (!name || !statKeys.includes(name.toUpperCase())){
+      return undefined
+    } else if (statKeys.includes(name.toUpperCase())) {
+      return 22
+    }
   }
 }
