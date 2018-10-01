b923878be4e519404a968d4b79e1afeb90ee950d
Kevin Simpson <relasine@gmail.com>
2018-10-01 16:52:50 -0600

    Complete It1 Test 4

--- src/helper.js
+++ src/helper.js
@@ -1,17 +1,25 @@
 export default class DistrictRepository {
   constructor(data) {
     this.stats = data.reduce((obj, point) => {
-      if (!obj[point.Location]) {
-        obj[point.Location] = {}
+      const { Location, TimeFrame, Data } = point;
+      if (!obj[Location]) {
+        obj[Location.toUpperCase()] = {location: Location.toUpperCase()}
+        obj[Location.toUpperCase()].scores = []
       }
-      obj[point.Location][point.TimeFrame] = point.Data
+      obj[Location.toUpperCase()].scores.push({[point.TimeFrame]: Data})
+      obj[Location.toUpperCase()].scores.sort((a, b) => {
+        return Object.keys(a) - Object.keys(b);
+      })
       return obj
     }, {})
   }
-}
 
-const findByName = (district) => {
-  if (!district) {
-    return undefined;
+  findByName = (query) => {
+    if (query === undefined) {
+      return undefined;
+    } else {
+      return this.stats[query.toUpperCase()]
+    }
   }
 }
+
