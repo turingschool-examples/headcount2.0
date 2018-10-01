6ab679f0ff1c5acd8754aa1dd4f303dff741466c
“Freddie <freddielev@gmail.com>
2018-10-01 15:07:02 -0600

    Complete iteration 0

--- src/helper.js
+++ src/helper.js
@@ -1,3 +1,22 @@
-export default class DistrictRepository {
+import KinderData from './data/kindergartners_in_full_day_program.js'
 
+export default class DistrictRepository {
+  constructor(dataSet) {
+    this.dataSet = dataSet
+    this.stats = this.makeStats()
+  }
+
+  makeStats() {
+    let obj = this.dataSet.reduce((statistic, thing) => {
+    if (!statistic[thing.Location]) {
+      statistic[thing.Location] = {
+      [thing.TimeFrame] : thing.Data
+      }
+    } else {
+      statistic[thing.Location][thing.TimeFrame] = thing.Data
+    }
+    return statistic
+  }, {})
+  return obj
+  }
 }
