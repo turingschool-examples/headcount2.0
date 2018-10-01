7a4f065d7773facb53ec1173d3d80861ef6e48d8
Chris Chateau <christopherchateau@gmail.com>
2018-10-01 15:41:23 -0600

    Condense data, pass iteration 0

--- src/helper.js
+++ src/helper.js
@@ -1,12 +1,18 @@
 export default class DistrictRepository {
-
   constructor(data) {
-    this.stats = data;
+    this.stats = this.condenseData(data);
   }
-  
-  render() {
-    return(
-      <div></div>
-    )
-  }
+  condenseData = data => {
+    return data.reduce((acc, school) => {
+      let district = school.Location.toUpperCase();
+      if (!acc[district]) {
+        acc[district] = {
+          years: {}
+        };
+      }
+      acc[district].years[school.TimeFrame] =
+        Math.round(1000 * school.Data) / 1000;
+      return acc;
+    }, {});
+  };
 }
