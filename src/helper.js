06735eb04cc09f214b67f51d6cc67757f84d1dea
bhayek12@gmail.com <bhayek12@gmail.com>
2018-10-01 15:42:41 -0600

    Pass all tests in iteration 0

--- src/helper.js
+++ src/helper.js
@@ -1,3 +1,30 @@
 export default class DistrictRepository {
-
+  constructor(stats){
+    this.stats = this.cleanData(stats)
+  }
+  cleanData = (stats) => {
+    return stats.reduce((acc, district) => {
+      if(!acc[district.Location]){
+        acc[district.Location] = {
+          [district.TimeFrame]: district.Data
+        } 
+    } else { 
+       Object.assign( acc[district.Location], {[district.TimeFrame]: district.Data})  
+    }
+      return acc 
+    }, {})
+  }
+  findByName = (name) => {
+    // console.log(this.stats)
+    if(name === undefined) {
+      return undefined
+    } else {
+    const correctLocation = Object.keys(this.stats).find(stat => { 
+      if(stat === name){
+        console.log(this.stats[name])
+        return this.stats[name]
+      } return correctLocation
+    }) 
+  } 
+}
 }
