803a661b503151f68139213bbd242bb43dccfb4a
Laura Shamus <lkshamus@gmail.com>
2018-10-01 15:18:36 -0600

    Add iteration 0 method called cleanData

--- src/helper.js
+++ src/helper.js
@@ -1,3 +1,30 @@
 export default class DistrictRepository {
+  constructor(stats) {
+    this.stats = this.cleanData(stats)
+  }
 
+  cleanData(stats){
+     return stats.reduce((acc, district) => {
+      if (!acc[district.Location]) {
+        acc[district.Location] = {
+          [district.TimeFrame] : district.Data
+        }
+      } else {
+        Object.assign(acc[district.Location], {[district.TimeFrame] : district.Data})
+      }
+    return acc
+    }, {})
+  }
+  findByName(name) {
+
+    if (name == Object.keys(this.stats).toUpperCase()) {
+      return true
+    } else {
+      return undefined
+    }
+
+  }
 }
+
+  
+
