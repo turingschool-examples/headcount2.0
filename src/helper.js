e2982d69a011522b8ed52d77a33fa14e5e760dea
marcusp619 <marcusp619@gmail.com>
2018-10-01 14:18:15 -0600

    Add formatStats function

--- src/helper.js
+++ src/helper.js
@@ -1,3 +1,16 @@
 export default class DistrictRepository {
+  constructor(stats) {
+    this.stats = this.formatStats(stats);
+  }
 
+  formatStats = stats => {
+    return stats.reduce((acc, stat) => {
+      if(acc[stat.Location]) {
+        acc[stat.Location][stat.TimeFrame] = stat.Data
+      } else {
+        acc[stat.Location] = {[stat.TimeFrame] : stat.Data}
+      }
+      return acc;
+    },{})
+  }
 }
