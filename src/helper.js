8a1f920435a22351808d0bdd972631dbf9bfa59a
Ahmad Kayyali <kayyali18@gmail.com>
2018-10-02 17:32:37 -0600

    Pass Iteration-1 part 1 tests

--- src/helper.js
+++ src/helper.js
@@ -2,17 +2,40 @@ export default class DistrictRepository {
     constructor (data) {
         this.data = data;
         this.stats = this.filterData();
+            // console.log (this.stats)  
     }
 
     filterData = () => {
         return this.data.reduce ((locations, entry) => {
-            let district = entry.Location.toLowerCase()
+            let district = entry.Location.toUpperCase()
     
-            if (district in locations) return locations
-            else locations[district] = {}
+            if (district in locations) {
+                locations[district].stats[entry.TimeFrame] = this.roundData(entry.Data)
+            }
+            else {
+                locations[district] = {
+                    location: district,
+                    stats: {
+                        [entry.TimeFrame]: this.roundData(entry.Data)
+                    }
+                }
+            }
 
             return locations
         }, {})
+    }
+
+    roundData = (entry) => {
+       return Math.round(1000 * entry) / 1000 || 0
+    }
+
+    findByName = (name) => {
+        if (!name) return undefined
+        return this.stats[name.toUpperCase()]
+    }
+
+    findAllMatches = (input) => {
+        if (!input) return this.stats
     }
 }
  
