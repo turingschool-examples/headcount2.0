b78eea22016d99d27082a6af0edb900ff4d822f2
Tobin Nelson <tobin.j.n@gmail.com>
2018-10-02 15:15:23 -0600

    Refactor this.stats to be an array with properties of location and stats for each district

--- src/helper.js
+++ src/helper.js
@@ -2,18 +2,43 @@
 export default class DistrictRepository {
   constructor(stats){
     this.stats = stats.reduce((districts, school) => {
-        
-        if(!districts[school.Location]){
-          districts[school.Location] = []
-        } 
-        districts[school.Location].push(school)
+      let statsObj ={}   
 
-      return districts
+      statsObj[school.TimeFrame] = school.Data
+      
+      if(!districts[school.Location]){
+        districts[school.Location] = {}
+        } 
+      districts[school.Location] = {
+        Location: school.Location,
+        stats: statsObj
+      }
+    return districts
     }, {})
   }
 
-  findByName() {
-    
+
+  findByName(name) {
+    if(!name){
+      return undefined
+    }
+
+
+
   }
 }
+  
+//     const matchName = Object.keys(this.stats).reduce((arr, district) => {
+//       let districtCapitalize = district.toUpperCase()
+//       let nameCapitalize = name.toUpperCase()
+
+//       if(districtCapitalize === nameCapitalize) {
+//        arr = this.stats[district]
+//       }
+//       return arr
+//     }, [])
+
+//   return matchName[0]
+//   }
+// }
 
