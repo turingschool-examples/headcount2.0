45e8d41e2cf494ed997e13f381dd232884afd1f3
Graham <g@masterofnone.com>
2018-10-02 16:44:53 -0600

    Create App component

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,35 @@
 import React, { Component } from 'react';
+import CardContainer from './CardContainer';
 import '../css/App.css';
+
+import kinderData from '../data/kindergartners_in_full_day_program.js';
+import DistrictRepository from './helper'
+
 
 class App extends Component {
   constructor() {
     super()
-    this.state({
-
-    })
+    this.state = {
+      cards: null
+    }
   }
+
+  componentDidMount() {
+    this.displayAllCards(kinderData);
+  }
+
+  displayAllCards = (data) => {
+    const helper = new DistrictRepository(data);
+    const cards = helper.findAllMatches();
+    this.setState({ cards })
+  }
+
   render() {
     return (
       <div>
         {/* <Search/>
-        <Compare/>
-        <Cards/> */}
+        <Compare/> */}
+        <CardContainer/>
       </div>
     );
   }
---
+++ src/components/CardContainer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CardContainer = (props) => {
+  return(
+    <div>
+      hey
+    </div>
+  )
+}
+
+export default CardContainer;
