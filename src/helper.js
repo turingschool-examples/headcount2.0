8190a1f84332eace83203debf43b319109c358a4
Alex Bruce <brucealmightyalex@yahoo.com>
2018-10-02 18:18:53 -0600

    Add complete DistrictRepository class and functional iteration-0 functionality

--- src/helper.js
+++ src/helper.js
@@ -1,3 +1,29 @@
-export default class DistrictRepository {
+import React, { Component } from 'react';
+// import kindergartners from './data/kindergartners_in_full_day_program.js';
+
+
+class DistrictRepository extends Component {
+  constructor(kindergartners) {
+    super();
+    // this.state = {}
+    this.kindergartners = kindergartners
+    this.stats = this.statsReduce()
+  }
+
+  statsReduce = (kindergartners) => {
+    return this.kindergartners.reduce((acc, currentData) => {
+      if (!acc[currentData.Location]) {
+        acc[currentData.Location] = { [currentData.TimeFrame]: currentData.Data }
+      } else {
+        acc[currentData.Location] = {
+          ...acc[currentData.Location],
+          [currentData.TimeFrame.toString()]: currentData.Data
+        }
+      }
+      return acc;
+    }, {})
+  }
 
 }
+
+export default DistrictRepository;
