0c5e6225f876db9059efe8d57174e10b12ba5e19
bhayek12@gmail.com <bhayek12@gmail.com>
2018-10-03 17:14:05 -0600

    Add visual indication to data

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import kinderData from './data/kindergartners_in_full_day_program.js';
 import DistrictRepository from './helper';
-import CardContainer from './CardContainer'
+import CardContainer from './CardContainer';
 import './App.css';
 
 class App extends Component {
--- src/SchoolCard.js
+++ src/SchoolCard.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 const SchoolCard = ({location, data}) => {
 return (
-  <div>
+  <div className="school-card">
     <h1>{Object.keys(data)}</h1>
     <ul>
-      {Object.keys(data).map(location=> Object.keys(data[location]).map(year => <li>{location}: {data[location][year]}</li>))}
+      {Object.keys(data).map(location => Object.keys(data[location]).map(year => <li className={data[location][year] < 0.5 ? 'isLess' : 'isMore'}>{year}: {data[location][year]}</li>))}
     </ul>
   </div>
   )
