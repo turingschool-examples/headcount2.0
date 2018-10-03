7b6c2bd9d44ba9d1d9a26549ae0319e80d2c9f00
Tobin Nelson <tobin.j.n@gmail.com>
2018-10-03 14:57:12 -0600

    Pass all tests for findByName and findByMatches

--- src/helper.js
+++ src/helper.js
@@ -2,103 +2,61 @@
 export default class DistrictRepository {
   constructor(stats){
     this.stats = stats.reduce((districts, school) => {
-    let statsObj ={
-      Location: school.Location.toUpperCase(),
-      Year: school.TimeFrame,
-      Data: school.Data
-    }   
- 
-      let upperCaseLocation = school.Location.toUpperCase()
-      if(!districts[upperCaseLocation]){
-        districts[upperCaseLocation] = []
-      } 
-      districts[upperCaseLocation].push(statsObj)
-      // console.log(school.Location.toUpperCase())
-    return districts
-  }, {})
-
-    // stats.reduce((districts, school) => {
-    //   let statsObj ={}   
-
-    //   statsObj[school.TimeFrame] = school.Data
-      
-    //   if(!districts[school.Location]){
-    //     districts[school.Location] = {}
-    //     } 
-    //   districts[school.Location] = {
-    //     Location: school.Location.toUpperCase(),
-    //     stats: statsObj
-    //   }
-    // return districts
-    // }, {})
-
-    // console.log(this.stats)
+      let statsObj ={
+        location: school.Location.toUpperCase(),
+        year: school.TimeFrame,
+        data: school.Data
+      }   
+   
+        let upperCaseLocation = school.Location.toUpperCase()
+        if(!districts[upperCaseLocation]){
+          districts[upperCaseLocation] = []
+        } 
+        districts[upperCaseLocation].push(statsObj)
+      return districts
+    }, {})
   }
-
-  // console.log(this.stats)
-
-
-  // findByName = (name) => {
-  //   // if ( name === null ){
-  //   //   return undefined
-  //   //   console.log('if')
-  //   // } 
-
-  //   // console.log(name)
-  //   // else {
-  //     let nameMatch = Object.keys(this.stats)
-  //       .find( district => {
-  //         return district === name}
-  //         )
-
-  //       // console.log(nameMatch)
-  //     return this.stats[nameMatch]
-  //   }
 
   findByName(name){
     if (name === undefined) {
       return undefined
     } else {
       let upperName = name.toUpperCase()
-      const nameMatch = Object.keys(this.stats).filter( district =>  district === upperName )
-      
+      const nameMatch = Object.keys(this.stats).find( district =>  district === upperName )
+
+      if(nameMatch === undefined){ return undefined }
+
       const yearlyData = this.stats[nameMatch].reduce((obj, element) => {
-          if(!obj[element.Year]){
-            obj[element.Year] = element.Data
+          if(typeof element.data !== 'number'){ element.data = 0}
+
+          if(!obj[element.year]){
+            obj[element.year] = Math.round(element.data * 1000)/1000 
           }
           return obj
         }, {})
 
       var matchObj = this.stats[nameMatch].reduce((obj, year) => {
         obj = {
-          location: year.Location,
+          location: year.location,
           stats: yearlyData
         }
         return obj
       }, {})
-      console.log(matchObj)
     }
   return matchObj 
   }
+
+  findAllMatches(name){
+    if(name === undefined){
+      return Object.entries(this.stats)
+    } else {
+      let upperName = name.toUpperCase()
+      var nameMatch = Object.keys(this.stats)
+        .filter( district =>  district.includes(upperName) )
+    }
+    return nameMatch
+  }
 }
 
-///make the location object in the findName function
 
-
-
-
-  
-//     const matchName = Object.keys(this.stats).reduce((arr, district) => {
-//       let districtCapitalize = district.toUpperCase()
-//       let nameCapitalize = name.toUpperCase()
-
-//       if(districtCapitalize === nameCapitalize) {
-//        arr = this.stats[district]
-//       }
-//       return arr
-//     }, [])
-
-//   return matchName[0]
-//   }
-// }
 
