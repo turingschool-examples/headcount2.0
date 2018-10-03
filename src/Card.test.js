7e0574accbb1ca136fb900760ef69defa37443e2
Cole Hart <cole@colehart.com>
2018-10-03 17:30:39 -0600

    Setup Card test structure and add snapshot test

---
+++ src/Card.test.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+import Card from './Card'
+
+describe('Card', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallow(<Card />)
+  })
+
+  it('should match the snapshot', () => {
+    expect(wrapper).toMatchSnapshot();
+  })
+})
