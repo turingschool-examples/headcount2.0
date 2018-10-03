34deb79b454e529cd0b4ce93800db3bdca13f03a
Ahmad Kayyali <kayyali18@gmail.com>
2018-10-03 13:01:44 -0600

    Clean up and set up plan to move ahead

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import React, { Component } from 'react';
+
+import kinderData from './data/kindergartners_in_full_day_program.js';
+import DistrictRepository from './helper.js'
 import './App.css';
 
+const district = new DistrictRepository (kinderData)
+
 class App extends Component {
+  constructor (props) {
+    super (props) 
+    this.state = {
+      data: district.findAllMatches()
+    }
+  }
   render() {
     return (
-      <div>Welcome To Headcount 2.0</div>
+      <div>
+        <h1>Welcome To Headcount 2</h1>
+        {district.findAllMatches().map (entry => {
+          let x = Object.keys(entry.stats)
+          return x.map (stat => {
+            return <p> {entry.stats[stat]}</p>
+          })
+        })}
+      </div>
     );
   }
 }
