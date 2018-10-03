ee1c04d3e1e9dd45c726bc3da371a470e21c96d0
Justin Stewart <Justinstewart3313@gmail.com>
2018-10-03 15:51:27 -0600

    Display year and schoolData as grouped together on DOM

--- src/Card.js
+++ src/Card.js
@@ -3,8 +3,15 @@ import React, { Component } from 'react'
 
 
 const Card = ({location, stats}) => {
+    const years = Object.keys(stats)
+    const schoolData = years.map( year => {
+      return <p>{year} : {stats[year]}</p>
+    })
     return (
-      <h3>{location}</h3>
+      <div>
+        <h3>{location}</h3>
+          {schoolData}
+      </div>
     )
   }
 
