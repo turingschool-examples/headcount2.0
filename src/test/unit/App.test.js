e81552a0fcc1f9fc95ea81c9baf9c2985feae230
marcusp619 <marcusp619@gmail.com>
2018-10-04 14:05:38 -0600

    Add test to check if all cards are rendered

--- src/test/unit/App.test.js
+++ src/test/unit/App.test.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { shallow } from 'enzyme';
-
 import App from '../../../src/App';
 
 describe('App', () => {
@@ -13,7 +12,7 @@ describe('App', () => {
 
   it('should have an array length of 181', () => {
     const wrapper = shallow(<App />);    
-    
+
     expect(wrapper.state('schoolData').length).toEqual(181);
   })
 
