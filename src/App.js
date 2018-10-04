5b4fb52cd4fd307bbc5e55aee92b0d74b1f3c10e
Chris Chateau <christopherchateau@gmail.com>
2018-10-03 19:46:45 -0600

    Add background image

--- src/App.js
+++ src/App.js
@@ -29,6 +29,7 @@ class App extends Component {
   render() {
     return (
       <div>
+        <h1 className="main-logo">head count</h1>
         <Search handleSearch={this.handleSearch} />
         <CardContainer cards={this.state.schoolData} />
       </div>
