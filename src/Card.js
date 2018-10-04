eff20acd2fa8bd67faef6a81ac3fdb7ff0062503
Chris Chateau <christopherchateau@gmail.com>
2018-10-03 19:03:38 -0600

    Add checkmark and cancel icons ot cards, fix prev bugs

--- src/Card.js
+++ src/Card.js
@@ -1,34 +1,29 @@
 import React from "react";
 import PropTypes from "prop-types";
 import checkIcon from "./checked.svg";
+import cancelIcon from "./cancel.svg";
 import "./Card.css";
 
 const Card = ({ location, stats }) => {
   const years = Object.keys(stats);
   const cardData = years.map(year => {
-    if (stats[year] > 0.5) {
-      return (
-        <p className={"green"} key={year}>
-        <img className="check-icon" src={checkIcon} />
-          {` ${year} : ${stats[year]}`}
-        </p>
-      );
-    } else {
-      return (
-        <p className={"red"} key={year}>
+    return (
+      <div>
+        <p className={stats[year] > 0.5 ? "green" : "red"} key={year}>
+          <img
+            className="icon"
+            src={stats[year] > 0.5 ? checkIcon : cancelIcon}
+          />
           {`${year} : ${stats[year]}`}
         </p>
-      );
-    }
-
-    <p className={"green"} key={year}>
-      {`${year} : ${stats[year]}`}
-    </p>;
+      </div>
+    );
   });
+
   return (
     <div className="card">
       <h3 className="school-name">{location}</h3>
-      <ul>{cardData}</ul>
+      {cardData}
     </div>
   );
 };
