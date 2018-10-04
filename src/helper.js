83c578f0c36e062b4b3cd231b044ce435dc0abe7
Alex Bruce <brucealmightyalex@yahoo.com>
2018-10-04 13:26:34 -0600

    Add fully functional findByName method

--- src/helper.js
+++ src/helper.js
@@ -13,15 +13,49 @@ class DistrictRepository extends Component {
   statsReduce = (kindergartners) => {
     return this.kindergartners.reduce((acc, currentData) => {
       if (!acc[currentData.Location]) {
-        acc[currentData.Location] = { [currentData.TimeFrame]: currentData.Data }
+        if (typeof (currentData.Data) !== 'string') {
+          acc[currentData.Location] = { [currentData.TimeFrame]: Math.round(currentData.Data * 1000) / 1000  }
+        } else {
+          acc[currentData.Location] = { [currentData.TimeFrame]: 0 }
+        }
       } else {
-        acc[currentData.Location] = {
-          ...acc[currentData.Location],
-          [currentData.TimeFrame.toString()]: currentData.Data
+        if (typeof (currentData.Data) !== 'string') {
+          acc[currentData.Location] = {
+            ...acc[currentData.Location],
+            [currentData.TimeFrame.toString()]: Math.round(currentData.Data * 1000) / 1000 }
+        } else {
+          acc[currentData.Location] = {
+            ...acc[currentData.Location],
+            [currentData.TimeFrame.toString()]: 0 
+          }
         }
       }
       return acc;
     }, {})
+  }
+
+  findByName = (name) => {
+    if (!name) {
+      return undefined;
+    } else {
+      let uppercaseName = name.toUpperCase();
+      let statKeys = Object.keys(this.stats)
+
+      let foundData = statKeys.reduce((foundItem, districtKey) => {
+        if (districtKey.toUpperCase() === uppercaseName) {
+          foundItem.location = districtKey.toUpperCase();
+          foundItem.stats = Object.assign({ ...this.stats[districtKey] }, {});
+        }
+        return foundItem;
+      }, {})
+
+
+      if (Object.keys(foundData).length) {
+        return foundData;
+      } else {
+        return undefined;
+      }
+    }
   }
 
 }
