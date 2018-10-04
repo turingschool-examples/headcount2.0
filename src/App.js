069d87a8622490b8f2b87e109bd451f57e32a911
Laura Shamus <lkshamus@gmail.com>
2018-10-04 09:07:43 -0600

    Add schoolk 's data onto screen without styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import React, { Component } from 'react';
 import './App.css';
-import './helper';
+import DistrictRepository from './helper';
+import './DataCard';
+import kinderData from './testData.js';
+import CardContainer from './CardContainer';
 
 class App extends Component {
+  constructor(props){
+    super(props)
+
+    let test = new DistrictRepository(kinderData)
+
+    this.state = {
+      schoolCards: test.findAllMatches()
+    }
+  }
+
+
   render() {
     return (
-      <div>Welcome To Headcount 2.0</div>
+      <div>Welcome To Headcount 2.0
+          <CardContainer schoolCards={this.state.schoolCards} />
+      </div>
     );
   }
 }
