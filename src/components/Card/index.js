52a97ce3c402ee912319e4c0af48290cc17f0993
Cole Hart <cole@colehart.com>
2018-10-06 14:34:34 -0600

    Add styling to Card

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -4,10 +4,10 @@ import './Card.css'
 const Card = ({location, data}) => {
   return(
     <article className="Card">
-      <h2>{location}</h2>
-      <ul>
+      <h2 className="Card-title">{location}</h2>
+      <ul className="Card-list">
         { Object.keys(data).map((year, index) => {
-          return (<li key={index}>
+          return (<li key={index} className="Card-entry">
             {year}: {data[year]}
           </li>)
           })
