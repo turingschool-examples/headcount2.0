4c3e0c8872187b30820e8d5ad5905153384a831b
Cole Hart <cole@colehart.com>
2018-10-06 14:40:32 -0600

    Add .low to <li> in Card if value is 0.5 or lower

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -7,14 +7,20 @@ const Card = ({location, data}) => {
       <h2 className="Card-title">{location}</h2>
       <ul className="Card-list">
         { Object.keys(data).map((year, index) => {
-          return (<li key={index} className="Card-entry">
-            {year}: {data[year]}
-          </li>)
+          return (
+            <li key={index}
+                className={`Card-entry ${checkLow(data, year)}`}>
+              {year}: {data[year]}
+            </li>)
           })
         }
       </ul>
     </article>
   )
+}
+
+const checkLow = (data, year) => {
+  if (data[year] >= 0.5) return 'low'
 }
 
 export default Card
