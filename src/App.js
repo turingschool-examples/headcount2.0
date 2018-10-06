939e30562b03c1099404ae95d2f8c542f38cb8c4
Alexander Ela <ela.alexander@gmail.com>
2018-10-06 01:38:54 -0600

    Add cap icon to header

--- src/App.js
+++ src/App.js
@@ -42,7 +42,10 @@ class App extends Component {
     return (
       <div className='App'>
       	<div className='header'>
-      		<h1 className='app-site-title'>Headcount</h1>
+
+      		<h1 className='app-site-title'>Headcount
+      			<span className='icon-span'><i className="fas fa-graduation-cap"></i>
+      			</span></h1>
       	</div>
       	<Search searchDistrict={this.searchDistrict}/>
       	<DistrictsContainer districts={data.findAllMatches(searchTerm)}/>
