93b11e832de1950ccecfd265ee2e60d7aadd56a3
Chris Chateau <christopherchateau@gmail.com>
2018-10-06 12:30:53 -0600

    Create missing test files

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { configure, shallow, mount } from "enzyme";
+import { shallow, mount } from "enzyme";
 import DistrictRepository from "./helper.js";
 import kinderData from "./data/kindergartners_in_full_day_program.js";
 import App from "./App";
@@ -13,6 +13,10 @@ describe("App component", () => {
   beforeEach(() => {
     wrapper = shallow(<App />);
   });
+
+  it('matches the snapshot', () => {
+    expect(wrapper).toMatchSnapshot()
+  })
 
   it("renders without crashing", () => {
     const div = document.createElement("div");
