6379163f5a41178533819d4bcab33c8b59957afa
Cole Hart <cole@colehart.com>
2018-10-06 15:11:32 -0600

    Add styling for .Search

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -2,9 +2,20 @@ import React, { Component } from 'react'
 import './Search.css'
 
 class Search extends Component {
+  constructor() {
+    super()
+    this.state = {
+      query: ''
+    }
+  }
+
   render() {
     return(
       <form className="Search">
+        <input
+          aria-label="Search for a specific district here"
+          placeholder="Search for a specific district"
+        />
       </form>
     )
   }
