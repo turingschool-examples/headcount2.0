ad7da84a8c8b152f36b408faafe52e57f7660f7b
Chris Chateau <christopherchateau@gmail.com>
2018-10-06 01:49:46 -0600

    Change arrow icon to pencil

--- src/ComparisonCard.js
+++ src/ComparisonCard.js
@@ -1,6 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
-import arrowIcon from "./images/arrow.svg";
+
+import pencilIcon from "./images/pencil.svg";
 import "./ComparisonCard.css";
 
 const ComparisonCard = props => {
@@ -9,7 +10,7 @@ const ComparisonCard = props => {
   return (
     <div className="comparison-card">
       <h3 className="avg-top">
-        <img className="arrow-icon" alt="arrow" src={arrowIcon} />
+        <img className="arrow-icon" alt="arrow" src={pencilIcon} />
         AVERAGE
       </h3>
       <p> {props[districtNames[0]]}</p>
@@ -17,7 +18,7 @@ const ComparisonCard = props => {
       <p> {props.compared}</p>
       <h3 className="avg-bottom">
         AVERAGE
-        <img className="arrow-icon rotate" alt="arrow" src={arrowIcon} />
+        <img className="arrow-icon rotate" alt="arrow" src={pencilIcon} />
       </h3>
       <p> {props[districtNames[1]]}</p>
     </div>
