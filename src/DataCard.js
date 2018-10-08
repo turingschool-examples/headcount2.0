df8b817d11ea966039d66943097e622dd0b387bf
Laura Shamus <lkshamus@gmail.com>
2018-10-08 09:30:55 -0600

    Add refactor for DataCard

--- src/DataCard.js
+++ src/DataCard.js
@@ -3,6 +3,9 @@ import PropTypes from 'prop-types';
 
 
 const DataCard = ({school, compareTwoCards}) => {
+
+const data = Object.keys(school.stats).map((year, index) => { return <li className={school.stats[year] < 0.5 ? 'less-than' : 'more-than' }>{year}: {school.stats[year]}</li> })
+
   return (
     <div 
       key={Date.now()}
@@ -11,52 +14,7 @@ const DataCard = ({school, compareTwoCards}) => {
     >
       <h1>{school.location}</h1>
       <ul>
-        <li 
-          className=
-            {Object.values(school.stats)[0] < 0.5 ? 'less-than' : 'more-than'}> 
-          2004: {Object.values(school.stats)[0]}</li>
-        <li 
-          className=
-            {Object.values(school.stats)[1] < 0.5 ? 'less-than' : 'more-than'}> 
-          2005: {Object.values(school.stats)[1]}</li>
-        <li 
-          className=
-            {Object.values(school.stats)[2] < 0.5 ? 'less-than' : 'more-than'}>
-           2006: {Object.values(school.stats)[2]}</li>
-        <li 
-          className=
-            {Object.values(school.stats)[3] < 0.5 ? 'less-than' : 'more-than'}>
-           2007: {Object.values(school.stats)[3]}</li>
-        <li 
-          className=
-            {Object.values(school.stats)[4] < 0.5 ? 'less-than' : 'more-than'}> 
-          2008: {Object.values(school.stats)[4]}</li>
-        <li 
-          className=
-            {Object.values(school.stats)[5] < 0.5 ? 'less-than' : 'more-than'}>
-           2009: {Object.values(school.stats)[5]}</li>
-        <li 
-          className=
-            {Object.values(school.stats)[6] < 0.5 ? 'less-than' : 'more-than'}> 
-          2010: {Object.values(school.stats)[6]}</li>
-        <li 
-          className=
-            {Object.values(school.stats)[7] < 0.5 ? 'less-than' : 'more-than'}> 
-          2011: {Object.values(school.stats)[7]}</li>
-        <li 
-          className=
-            {Object.values(school.stats)[8] < 0.5 ? 'less-than' : 'more-than'}> 
-          2012: 
-          {Object.values(school.stats)[8]}</li>
-        <li 
-          className=
-            {Object.values(school.stats)[9] < 0.5 ? 'less-than' : 'more-than'}> 
-          2013: {Object.values(school.stats)[9]}</li>
-        <li 
-          className=
-            {Object.values(school.stats)[10] < 0.5 ? 'less-than' : 'more-than'}
-            > 
-          2014: {Object.values(school.stats)[10]}</li>
+        <li>{data}</li>
       </ul>
     </div>
   );
