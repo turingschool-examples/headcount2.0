d6d06b567427ad2d72e74ba0b21a36e6b7eb3448
Chris Chateau <christopherchateau@gmail.com>
2018-10-07 18:57:45 -0600

    Wipe README.md

--- src/BlankCard.test.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import BlankCard from "./BlankCard";
-import { shallow } from "enzyme";
-
-it("matches the snapshot", () => {
-  const wrapper = shallow(<BlankCard />);
-  expect(wrapper).toMatchSnapshot();
-});
