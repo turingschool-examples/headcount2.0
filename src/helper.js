ea6d65a66de8c17f1c1511e99d45a937467403a3
Sara Ebbers <johndoe@example.com>
2018-11-26 14:58:02 -0700

    Create DistrictRepository constructor, add cleanStats method.  Passing tests for iteration zero

--- src/helper.js
+++ src/helper.js
@@ -1,3 +1,14 @@
 export default class DistrictRepository {
+  constructor(stats) {
+    this.stats = this.cleanStats(stats);
+  }
+
+  cleanStats = (stats) => stats.reduce((allStats, stat) => {
+    allStats[stat.Location] = {[stat.TimeFrame]: stat.Data};
+    console.log(Object.keys(allStats).length);
+    return allStats;
+  }, {});
+
 
 }
+
