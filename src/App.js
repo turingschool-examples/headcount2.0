fe78f412dab4d6b39f7f9c8e84acc9b343aa72f8
Ashley Levi <Ashplevi@gmail.com>
2018-11-27 13:11:21 -0700

    Finish iteration one

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import React, { Component } from 'react';
 import './App.css';
+import Data from './data/kindergartners_in_full_day_program.js';
+import DistrictRepository from './helper.js';
 
 class App extends Component {
+  constructor(){
+    super()
+    this.state = {
+      data: {}
+    }
+  }
+
+  // componentDidMount = () => {
+  //   this.setState({
+  //     data: Data 
+  //   })
+  // }
+
   render() {
     return (
-      <div>Welcome To Headcount 2.0</div>
+      <div>Welcome To Headcount 2.0
+      </div>
+
     );
   }
 }
--- src/helper.js
+++ src/helper.js
@@ -1,3 +1,42 @@
-export default class DistrictRepository {
 
+export default class DistrictRepository {
+  constructor(data) {
+    this.stats = data.reduce((obj, currElement) => {
+      let upperCaseLoc = currElement.Location.toUpperCase();
+      if (!obj[upperCaseLoc]) {
+        obj[upperCaseLoc] = {}
+      } 
+        let value;
+        if (!isNaN(currElement.Data)) {
+          value = Math.round(1000*currElement.Data)/1000
+        } else {
+          value = 0
+        }
+        obj[upperCaseLoc].stats = {...obj[upperCaseLoc].stats, [currElement.TimeFrame] : value };
+        obj[upperCaseLoc].location = upperCaseLoc;
+      return obj;
+    }, {})
+  }
+
+  findByName = (location) => {
+    if (!location) {
+      return 
+    }
+    let upperCaseLoc = location.toUpperCase();
+      return this.stats[upperCaseLoc]
+  }
+
+  findAllMatches = (location) => {
+    let keys = Object.keys(this.stats)
+    if (!location) {
+      return keys
+    } else {
+      return keys.filter((currElement) => {
+        let upperCaseLoc = location.toUpperCase();
+          if (currElement.includes(upperCaseLoc)) {
+            return currElement
+          }
+      })
+    } 
+}
 }
