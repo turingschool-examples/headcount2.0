40ab7adf28bd5d3c222481cd5e798133c9fd385c
Sara Ebbers <johndoe@example.com>
2018-11-27 15:10:21 -0700

    Create CardContainer Component with anticipated imports and return jsx

---
+++ src/CardContainer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Card from './Card';
+
+const CardContainer = () => {
+
+  return(
+    <div>
+      <h3>Data for Each District Contained Here</h3>
+    </div>
+  )
+}
+
+export default CardContainer;
