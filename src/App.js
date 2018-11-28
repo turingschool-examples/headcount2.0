f15bade5ae186caff7aaa91aee325b1368c82cfe
Sara Ebbers <johndoe@example.com>
2018-11-28 15:28:39 -0700

    Add ComponentDidMount and prepareDataForDisplay methods to App.js

--- src/App.js
+++ src/App.js
@@ -10,17 +10,35 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      dataSet: new DistrictRepository(kinderData),
+      dataSet: '',
+      displayData: [],
+      searchWord: ''
     }
   }
 
+  componentDidMount() {
+    const dataSet = kinderData;
+    this.setState({ dataSet });
+    this.prepareDataForDisplay(dataSet)
+  }
+
+  prepareDataForDisplay = (dataSet) => {
+    const cleanData = new DistrictRepository(dataSet);
+    const displayData = cleanData.findAllMatches(this.state.searchWord);
+    this.setState({ displayData })
+  }
+
   render() {
+
+    const { cleanData, displayData } = this.state;
+
     return (
       <div>
         <h1>Welcome To Headcount 2.0</h1>
         <Search />
         <ComparisonContainer />
-        <CardContainer />
+        <CardContainer 
+          displayData={ displayData }/>
       </div>
     );
   }
--- src/CardContainer.js
+++ src/CardContainer.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import Card from './Card';
+import PropTypes from 'prop-types';
 
-const CardContainer = () => {
+const CardContainer = ( {displayData} ) => {
+
+  const cards = displayData.map(district => {
+    return <Card key={Date.now()} {...district} />
+  })
 
   return(
     <div>
       <h3>Data for Each District Contained Here</h3>
+      <section>
+        { cards }
+      </section>
     </div>
   )
+}
+
+CardContainer.propTypes = {
+  dataSet: PropTypes.array.isRequired
 }
 
 export default CardContainer;
