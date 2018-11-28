0d42770bf02bb4ee08b8d7686dc8d0f97bcaa78d
Cole Vanacore <colevanacore@gmail.com>
2018-11-28 14:58:35 -0700

    Complete iteration 4

--- src/helper.js
+++ src/helper.js
@@ -35,4 +35,24 @@ export default class DistrictRepository {
       });
     }
   }
+
+  findAverage = (location) => {
+    const values = Object.values(this.stats[location.toUpperCase()].stats);
+    const average = values.reduce((acc, currNum) => {
+      acc += currNum;
+      return acc;
+    },0)/(values.length);
+    return Math.round(1000*average)/1000;
+  }
+
+  compareDistrictAverages = (loc1, loc2) => {
+    const loc1Avg = this.findAverage(loc1);
+    const loc2Avg = this.findAverage(loc2);
+    let result = {};
+    result[loc1.toUpperCase()] = loc1Avg;
+    result[loc2.toUpperCase()] = loc2Avg;
+    result.compared = Math.round(1000*loc1Avg/loc2Avg)/1000;
+    return result;
+  }
+  
 }
